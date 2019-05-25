94d89029155b1270b388ca15086b8ac3eaca98fb
Robert Bergers <bergersr@my.easternct.edu>
2019-05-25 13:04:31 -0400

    notetaking app

---
+++ node/notes-app/app.js
@@ -0,0 +1,10 @@
+const validator = require('validator')
+const getNotes = require('./notes.js')
+
+
+console.log(validator.isURL('https://bergers.rocks'))
+
+//console.log(getNotes())
+//const [name, add] = require('./utils.js')
+//console.log(name)
+//console.log(add(5,6))
---
+++ node/notes-app/notes.js
@@ -0,0 +1,3 @@
+const addNote = () => {return 'Here\'s ur notes buddee!'}
+
+module.exports = addNote
