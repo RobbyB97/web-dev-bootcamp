8c676b0ca22c968af839bbd6dc4a73d5b4234c71
Robert Bergers <bergersr@my.easternct.edu>
2019-05-25 13:54:48 -0400

    add commands with yargs

--- node/notes-app/app.js
+++ node/notes-app/app.js
@@ -1,10 +1,44 @@
-const validator = require('validator')
+const chalk = require('chalk')
+const yargs = require('yargs')
 const getNotes = require('./notes.js')
 
+// yargs version
+yargs.version('1.100.4325')
 
-console.log(validator.isURL('https://bergers.rocks'))
+// add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  handler: function () {
+    console.log('Adding new note')
+  }
+})
 
-//console.log(getNotes())
-//const [name, add] = require('./utils.js')
-//console.log(name)
-//console.log(add(5,6))
+// remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove an old note',
+  handler: function () {
+    console.log('Removing old note!')
+  }
+})
+
+// list command
+yargs.command({
+  command: 'list',
+  describe: 'List notes',
+  handler: function () {
+    console.log('Listing notes!')
+  }
+})
+
+// read command
+yargs.command({
+  command: 'read',
+  describe: 'Read notes',
+  handler: function () {
+    console.log('Reading note!')
+  }
+})
+
+console.log(yargs.argv)
