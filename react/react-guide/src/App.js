8ebfe2c1a04dbb4892bb6422a9acd83d8e8832a2
Robert Bergers <bergersr@my.easternct.edu>
2019-05-27 09:27:29 -0400

    add styles to Person react component

--- react/react-guide/src/App.js
+++ react/react-guide/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import './Person/Person.css'
 import Person from './Person/Person'
 
 class App extends Component {
@@ -18,16 +19,25 @@ class App extends Component {
   //    {name: 'NO WHEY', age: 5043}
   //  ],
   //}
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     //alert('OMG I BEEN CLIKT!')
     this.setState({
       persons: [
-        {name: 'Dude', age: 90},
+        {name: newName, age: 90},
         {name: 'BRO', age: 53},
         //{name: 'OMG', age: 1337},
         //{name: 'NO WHEY', age: 5043}
       ]
-    });
+    } )
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        {name: event.target.value, age: 90},
+        {name: 'BRO', age: 53}
+      ]
+    } )
   }
 
   render() {
@@ -35,11 +45,12 @@ class App extends Component {
       <div className="App">
         <h1> Im a React App </h1>
         <p> It is working! </p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <button onClick={() => this.switchNameHandler('JEEBIS!')}>Switch Name</button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}
-          click={this.switchNameHandler} 
+          click={this.switchNameHandler.bind(this, 'HOLY COW!')}
+          changed={this.nameChangedHandler}
         />
         <Person
           name={this.state.persons[1].name}
--- react/react-guide/src/Person/Person.js
+++ react/react-guide/src/Person/Person.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 const person = (props) => {
   return (
-    <div>
+    <div className="Person">
       <p onClick={props.click}>I am {props.name}! 2 + 2 = {props.age}</p>
       <p>{props.children}</p>
+      <input type="text" onChange={props.changed} value={props.name}/>
     </div>
   );
 }
