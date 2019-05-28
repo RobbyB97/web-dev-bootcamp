cc18d609354db46245314a108df2ac4a586f27e7
Robert Bergers <bergersr@my.easternct.edu>
2019-05-28 08:47:42 -0400

    add ValidComponent

--- react/assignment-two/src/App.js
+++ react/assignment-two/src/App.js
@@ -1,14 +1,22 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ValidComponent from './ValidComponent/ValidComponent'
 
 class App extends Component {
   state = {
     inpLength: 0,
+    outText: 'Text too short!'
   }
 
   changeInpNum = (event) => {
     this.setState({inpLength: event.target.value.length})
+
+    if (this.state.inpLength < 5) {
+      this.setState({outText: 'Text too short!'})
+    } else {
+      this.setState({outText: 'Text long enough!'})
+    }
   }
 
   render() {
@@ -19,6 +27,9 @@ class App extends Component {
           onChange = {(event) => this.changeInpNum(event)}
         />
         <p> {this.state.inpLength} </p>
+        <ValidComponent
+          text = {this.state.outText}
+        />
       </div>
     );
   }
--- react/assignment-two/src/ValidComponent/ValidComponent.js
+++ react/assignment-two/src/ValidComponent/ValidComponent.js
@@ -3,7 +3,7 @@ import React from 'react'
 const validComponent = (props) => {
   return (
     <div className="ValidComponent">
-
+      <p>{props.text}</p>
     </div>
   )
 }
