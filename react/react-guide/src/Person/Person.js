98812537e4bd70e702eed17843d3e257ad5d9a28
Robert Bergers <bergersr@my.easternct.edu>
2019-05-28 12:38:02 -0400

    add media query for size

--- react/react-guide/src/Person/Person.js
+++ react/react-guide/src/Person/Person.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React from 'react'
+import Radium from 'radium'
 import './Person.css'
 
 const person = (props) => {
+  const style = {
+    '@media (min-width: 500px)': {
+      width: '450px'
+    }
+  }
+
   return (
-    <div className="Person">
+    <div className="Person" style={style}>
       <p onClick={props.click}>I am {props.name}! 2 + 2 = {props.age}</p>
       <p>{props.children}</p>
       <input
@@ -15,4 +22,4 @@ const person = (props) => {
   );
 }
 
-export default person;
+export default Radium(person);
