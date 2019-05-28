04e1f21f75c4bcac8b457ded1fd84d4dd6129e20
Robert Bergers <bergersr@my.easternct.edu>
2019-05-28 14:25:57 -0400

    generate url for darksky api by pulling key from secretkey file

--- node/weather-app/app.js
+++ node/weather-app/app.js
@@ -1,6 +1,10 @@
 const request = require('request')
 const fs = require('fs')
 
-const getKey = () => {return fs.readFileSync('./secretkey.txt').toString()}
+const getKey = () => {return fs.readFileSync('./secretkey.txt').toString().replace(' ', '')}
+const geturl = () => {return 'https://api.darksky.net/forecast/' + getKey() + '/37.8267,-122.4233'}
+const url = geturl().replace('\n', '')
 
-console.log(getKey())
+request({url: url}, (error, response) => {
+  console.log(response)
+})
