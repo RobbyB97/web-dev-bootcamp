28047d0c2ae8e9d71a1e50f7c22e8c7cb255a878
Robert Bergers <bergersr@my.easternct.edu>
2019-05-28 17:54:20 -0400

    error handling

--- node/weather-app/app.js
+++ node/weather-app/app.js
@@ -8,15 +8,30 @@ const darkSkyurl = getDSurl().replace('\n', '')
 
 // Get mapbox info
 const mapBoxKey = () => {return fs.readFileSync('./mapboxkey.txt').toString().replace(' ', '')}
-const getMBurl = () => {return 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=' + mapBoxKey() + '&limit=1'}
+const getMBurl = () => {return 'https://api.mapbox.com/geocoding/v5/mapbox.places/North%20Haven.json?access_token=' + mapBoxKey() + '&limit=1'}
 const mapBoxurl = getMBurl().replace('\n', '')
 
-//request({url: darkSkyurl, json: true}, (error, response) => {
-//  console.log(response.body.currently)
-//  let temp = response.body.currently.temperature.toString()
-//  let rainChance = response.body.currently.precipProbability.toString()
-//  console.log(response.body.daily.data[0].summary + ' It is currently ' + temp + ' degrees out. There is a ' + rainChance + '% chance of rain.')
-//})
+request({url: darkSkyurl, json: true}, (error, response) => {
+  if (error) {
+    console.log('Could not reach DarkSky')
+  } else if (response.body.error) {
+    console.log('Something went wrong with DarkSky')
+  } else {
+    const temp = response.body.currently.temperature.toString()
+    const rainChance = response.body.currently.precipProbability.toString()
+    console.log(response.body.daily.data[0].summary + ' It is currently ' + temp + ' degrees out. There is a ' + rainChance + '% chance of rain.')
+  }
+})
+
 request({url: mapBoxurl, json: true}, (error, response) => {
-  console.log(response.body.features[0].geometry['coordinates'])
+  if (error) {
+    console.log('Could not reach MapBox')
+  } else if (!response.body.features[0]) {
+    console.log('Could not find location')
+  } else {
+    const coordinates = response.body.features[0].geometry['coordinates']
+    const latitude = coordinates[1]
+    const longitude = coordinates[0]
+    console.log('Latitude: ' + latitude + '\nLongitude: ' + longitude)
+  }
 })
