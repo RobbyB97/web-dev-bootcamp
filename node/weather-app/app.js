b4a6f75b67dc6d19bcf73dede591f24d2482c577
Robert Bergers <bergersr@my.easternct.edu>
2019-05-28 14:56:35 -0400

    parse and log data from darksky api

--- node/weather-app/app.js
+++ node/weather-app/app.js
@@ -5,6 +5,9 @@ const getKey = () => {return fs.readFileSync('./secretkey.txt').toString().repla
 const geturl = () => {return 'https://api.darksky.net/forecast/' + getKey() + '/37.8267,-122.4233'}
 const url = geturl().replace('\n', '')
 
-request({url: url}, (error, response) => {
-  console.log(response)
+request({url: url, json: true}, (error, response) => {
+  console.log(response.body.currently)
+  let temp = response.body.currently.temperature.toString()
+  let rainChance = response.body.currently.precipProbability.toString()
+  console.log(response.body.daily.data[0].summary + ' It is currently ' + temp + ' degrees out. There is a ' + rainChance + '% chance of rain.')
 })
