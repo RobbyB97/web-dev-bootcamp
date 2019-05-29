48f7d4974c860b844516935590c19ca0df3ec4b9
Robert Bergers <bergersr@my.easternct.edu>
2019-05-28 21:08:57 -0400

    move darksky to dedicated file

--- node/weather-app/app.js
+++ node/weather-app/app.js
@@ -1,30 +1,28 @@
 const request = require('request')
 const fs = require('fs')
 const geocode = require('./utils/mapbox')
+const weather = require('./utils/darksky')
 
-// Get darksky info
-const darkSkyKey = () => {return fs.readFileSync('./darkskykey.txt').toString().replace(' ', '')}
-const getDSurl = () => {return 'https://api.darksky.net/forecast/' + darkSkyKey() + '/37.8267,-122.4233'}
-const darkSkyurl = getDSurl().replace('\n', '')
-
-request({url: darkSkyurl, json: true}, (error, response) => {
-  if (error) {
-    console.log('Could not reach DarkSky')
-  } else if (response.body.error) {
-    console.log('Something went wrong with DarkSky')
-  } else {
-    const temp = response.body.currently.temperature.toString()
-    const rainChance = response.body.currently.precipProbability.toString()
-    console.log(response.body.daily.data[0].summary + ' It is currently ' + temp + ' degrees out. There is a ' + rainChance + '% chance of rain.')
-  }
-})
-
-geocode('Philadelphia', (error, data) => {
+const place = geocode('North Haven', (error, data) => {
   if (error) {
     console.log('ERROR: ' + error)
   } else if (data) {
     console.log(data)
+    callback(data)
   } else {
     console.log('Jeez idk you messed up man')
+  }
+})
+
+console.log(place)
+
+const forecast = weather(place, (error, data) => {
+  if (error) {
+    console.log('ERROR: ' + error)
+  } else if (data) {
+    console.log(data)
+    callback(data)
+  } else {
+    console.log('idk man')
   }
 })
---
+++ node/weather-app/utils/darksky.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+const request = require('request')
+
+const weather = (coordinates, callback) => {
+
+  // Generate url
+  const key = fs.readFileSync('./darkskykey.txt').toString().replace(' ', '')
+//  const coords = '/' + coordinates['latitude'] + ',' + coordinates['longitude']
+  let url = 'https://api.darksky.net/forecast/' + key + '/37.8267,-122.4233'
+  url = url.replace('\n', '')
+
+  // Request data from darksky
+  request({url: url, json: true}, (error, response) => {
+    if (error) {
+      console.log('Could not reach DarkSky')
+    } else if (response.body.error) {
+      console.log('Something went wrong with DarkSky')
+    } else {
+      const temp = response.body.currently.temperature.toString()
+      const rainChance = response.body.currently.precipProbability.toString()
+      callback(response.body.daily.data[0].summary + ' It is currently ' + temp + ' degrees out. There is a ' + rainChance + '% chance of rain.')
+    }
+  })
+}
+
+module.exports = weather
