324f59290d271c623796d8cf57def074b4e8d6bf
Robert Bergers <bergersr@my.easternct.edu>
2019-05-28 20:05:04 -0400

    make mapbox request with user input in callback function

--- node/weather-app/app.js
+++ node/weather-app/app.js
@@ -6,11 +6,6 @@ const darkSkyKey = () => {return fs.readFileSync('./darkskykey.txt').toString().
 const getDSurl = () => {return 'https://api.darksky.net/forecast/' + darkSkyKey() + '/37.8267,-122.4233'}
 const darkSkyurl = getDSurl().replace('\n', '')
 
-// Get mapbox info
-const mapBoxKey = () => {return fs.readFileSync('./mapboxkey.txt').toString().replace(' ', '')}
-const getMBurl = () => {return 'https://api.mapbox.com/geocoding/v5/mapbox.places/North%20Haven.json?access_token=' + mapBoxKey() + '&limit=1'}
-const mapBoxurl = getMBurl().replace('\n', '')
-
 request({url: darkSkyurl, json: true}, (error, response) => {
   if (error) {
     console.log('Could not reach DarkSky')
@@ -23,15 +18,38 @@ request({url: darkSkyurl, json: true}, (error, response) => {
   }
 })
 
-request({url: mapBoxurl, json: true}, (error, response) => {
+
+const geocode = (address, callback) => {
+
+  // Generate url
+  const key = fs.readFileSync('./mapboxkey.txt').toString().replace(' ', '')
+  let url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=' + key + '&limit=1'
+  url = url.replace('\n', '')
+
+  // Request data from mapbox
+  request({url: url, json: true}, (error, response) => {
+    if (error) {
+      callback('Unable to connect to mapbox...', undefined)
+    } else if (!response.body.features[0]) {
+      callback('Could not find location...', undefined)
+    } else {
+      const coordinates = response.body.features[0].geometry['coordinates']
+      const latitude = coordinates[1]
+      const longitude = coordinates[0]
+      callback(undefined, {
+        latitude: latitude,
+        longitude: longitude
+      })
+    }
+  })
+}
+
+geocode('Philadelphia', (error, data) => {
   if (error) {
-    console.log('Could not reach MapBox')
-  } else if (!response.body.features[0]) {
-    console.log('Could not find location')
+    console.log('ERROR: ' + error)
+  } else if (data) {
+    console.log(data)
   } else {
-    const coordinates = response.body.features[0].geometry['coordinates']
-    const latitude = coordinates[1]
-    const longitude = coordinates[0]
-    console.log('Latitude: ' + latitude + '\nLongitude: ' + longitude)
+    console.log('Jeez idk you messed up man')
   }
 })
