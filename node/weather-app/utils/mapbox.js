f038a0583974041fe3f545c8d53258be81a47085
Robert Bergers <bergersr@my.easternct.edu>
2019-05-28 20:09:12 -0400

    put mapbox api callback in dedicated file

---
+++ node/weather-app/utils/mapbox.js
@@ -0,0 +1,37 @@
+const fs = require('fs')
+const request = require('request')
+
+const geocode = (address, callback) => {
+
+  // Generate url
+  const key = fs.readFileSync('../mapboxkey.txt').toString().replace(' ', '')
+  let url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=' + key + '&limit=1'
+  url = url.replace('\n', '')
+
+  // Request data from mapbox
+  request({url: url, json: true}, (error, response) => {
+    if (error) {
+      callback('Unable to connect to mapbox...', undefined)
+    } else if (!response.body.features[0]) {
+      callback('Could not find location...', undefined)
+    } else {
+      const coordinates = response.body.features[0].geometry['coordinates']
+      const latitude = coordinates[1]
+      const longitude = coordinates[0]
+      callback(undefined, {
+        latitude: latitude,
+        longitude: longitude
+      })
+    }
+  })
+}
+
+geocode('Philadelphia', (error, data) => {
+  if (error) {
+    console.log('ERROR: ' + error)
+  } else if (data) {
+    console.log(data)
+  } else {
+    console.log('Jeez idk you messed up man')
+  }
+})
