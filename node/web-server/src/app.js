96683239f6f24c255942fcc307347afd972aa888
Robert Bergers <bergersr@my.easternct.edu>
2019-05-30 10:18:07 -0400

    render static html file from public directory on homepage

--- node/web-server/src/app.js
+++ node/web-server/src/app.js
@@ -1,22 +1,36 @@
 // libraries
+const path = require('path')
 const express = require('express')
 
+const publicPage = path.join(__dirname, '../public')
+
 const app = express()
+
+app.use(express.static(publicPage))
 
 app.get('', (req, res) => {
   res.send('<h1>CONTENT!</h1>')
 })
 
 app.get('/help', (req, res) => {
-  res.send('Hello express/help!')
+  res.send([{
+    name: 'Robby',
+    age: 22
+  },
+{
+  name: 'Ribby'
+}])
 })
 
 app.get('/about', (req, res) => {
-  res.send('This is about page!')
+  res.send('<h1>This is about page!</h1>')
 })
 
 app.get('/weather', (req, res) => {
-  res.send('<head><title>COOL</title></head><body><p>cool</p></body>')
+  res.send({
+    forecast: 'It is rainy',
+    location: 'North Haven'
+  })
 })
 
 app.get('/*', (req, res) => {
