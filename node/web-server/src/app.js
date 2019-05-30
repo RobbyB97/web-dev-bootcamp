7729ef61c58c9fde6e6e248885370a38b65ed409
Robert Bergers <bergersr@my.easternct.edu>
2019-05-30 14:01:08 -0400

    render dynamic pages

--- node/web-server/src/app.js
+++ node/web-server/src/app.js
@@ -1,12 +1,36 @@
 // libraries
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
 const publicPage = path.join(__dirname, '../public')
+
 app.set('view engine', 'hbs')
 app.use(express.static(publicPage))
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather App',
+    name: 'Robby'
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about/index', {
+    title: 'The About page',
+    name: 'Ribby'
+  })
+})
+
+app.get('/help', (req, res) => {
+  res.render('help/index', {
+    title: 'The Help page',
+    name: 'Ruby',
+    message: 'I can\'t help you I need help meself!'
+  })
+})
 
 app.get('/weather', (req, res) => {
   res.send({
