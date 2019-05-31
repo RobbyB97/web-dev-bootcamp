971736386adc329ffa64e7ccd9e9e1d7929360ad
Robert Bergers <bergersr@my.easternct.edu>
2019-05-31 15:58:09 -0400

    ES6 default parameters for objects

--- node/playground/7-default-params.js
+++ node/playground/7-default-params.js
@@ -2,4 +2,13 @@ const greet = (name='John') => {
   console.log('Hello ' + name)
 }
 
-greet('BIC')
+greet('Robby')
+
+const transaction = (type, {label, stock=0} = {}) => {
+  console.log(type, label, stock)
+}
+product = {
+  label: 'TEST'
+}
+
+transaction('order', product)
