50e8159b16196114da27280002d800d7be9cca33
Robert Bergers <bergersr@my.easternct.edu>
2019-06-02 16:18:29 -0400

    use fetch to retrieve json data from another website

--- node/web-server/public/js/app.js
+++ node/web-server/public/js/app.js
@@ -1,1 +1,7 @@
-alert('js/app.js')
+console.log('js/app.js is loaded')
+
+fetch('http://puzzle.mead.io/puzzle').then((response) => {
+  response.json().then((data) => {
+    console.log(data)
+  })
+})
