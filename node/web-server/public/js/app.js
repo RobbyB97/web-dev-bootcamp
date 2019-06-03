ba6bddf4b2f142328b718a1fac12b33870d321e0
Robert Bergers <bergersr@my.easternct.edu>
2019-06-03 14:38:51 -0400

    return error if query did not return location and forecast

--- node/web-server/public/js/app.js
+++ node/web-server/public/js/app.js
@@ -8,7 +8,11 @@ fetch('http://puzzle.mead.io/puzzle').then((response) => {
 
 fetch('http://localhost:3000/weather?address=boston').then((response) => {
   response.json().then((data) => {
-    console.log(data)
-    alert(data.location)
+    if (data.error) {
+      console.log(data.error)
+    } else {
+      console.log(data.location)
+      console.log(data.data)
+    }
   })
 })
