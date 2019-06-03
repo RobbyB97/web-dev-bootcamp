8a4196d264d7a77e04a0033342baea85d85aefe6
Robert Bergers <bergersr@my.easternct.edu>
2019-06-03 15:13:08 -0400

    return output of query in selected elements

--- node/web-server/public/js/app.js
+++ node/web-server/public/js/app.js
@@ -1,7 +1,10 @@
 console.log('js/app.js is loaded')
 
+// Select elements of page
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+const pOne = document.querySelector('#pOne')
+const pTwo = document.querySelector('#pTwo')
 
 weatherForm.addEventListener('submit', (e) => {
   e.preventDefault()  // Prevent page from refreshing
@@ -18,9 +21,11 @@ weatherForm.addEventListener('submit', (e) => {
       console.log('Dynamic query')
       if (data.error) {
         console.log(data.error)
+        pOne.innerHTML = data.error
+        pTwo.innerHTML = ''
       } else {
-        console.log(data.location)
-        console.log(data.data)
+        pOne.innerHTML = data.location
+        pTwo.innerHTML = data.data
       }
       console.log('End dynamic query')
     })
