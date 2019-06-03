60fed31849bd8675bfe120c7245ae475df55fc86
Robert Bergers <bergersr@my.easternct.edu>
2019-06-03 14:57:35 -0400

    dynamically generate query from form and return json object

--- node/web-server/public/js/app.js
+++ node/web-server/public/js/app.js
@@ -18,6 +18,30 @@ fetch('http://localhost:3000/weather?address=boston').then((response) => {
 })
 
 const weatherForm = document.querySelector('form')
-weatherForm.addEventListener('submit', () => {
+const search = document.querySelector('input')
+
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault()  // Prevent page from refreshing
+
+  const location = search.value
+  console.log(location)
+
+  // Generate query URL
+  const queryURL = 'http://localhost:3000/weather?address=' + location
+
+  // Fetch data and log json object
+  fetch(queryURL).then((response) => {
+    response.json().then((data) => {
+      console.log('Dynamic query')
+      if (data.error) {
+        console.log(data.error)
+      } else {
+        console.log(data.location)
+        console.log(data.data)
+      }
+      console.log('End dynamic query')
+    })
+  })
+
   console.log('Form submitted!')
 })
