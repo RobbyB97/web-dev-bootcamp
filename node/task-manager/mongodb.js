ece568fd626f88e8eb1b31f21518dcbaf8e0eb5f
Robert Bergers <bergersr@my.easternct.edu>
2019-06-05 17:34:12 -0400

    use updateOne promise to change document fields

--- node/task-manager/mongodb.js
+++ node/task-manager/mongodb.js
@@ -17,7 +17,26 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
 
   const db = client.db(databaseName)
 
+  // List all collections since Robo 3T doesn't work well on linux
   db.collection('tasks').find({}).toArray((error, tasks) => {
+    console.log('Tasks:')
     console.log(tasks)
   })
+  db.collection('users').find({}).toArray((error, users) => {
+    console.log('Users:')
+    console.log(users)
+  })
+
+  db.collection('users').updateOne({
+    _id: new ObjectID('5cf6c08f1e968d6fe62df761')
+  }, {
+    $set: {
+      name: 'Zeke'
+    }
+  }).then((result) => {
+    console.log('Success')
+  }).catch((error) => {
+    console.log('Error')
+  })
+
 })
