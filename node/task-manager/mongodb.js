374b4ad643b6e335b83ebf8bd1525d7bd31338d2
Robert Bergers <bergersr@my.easternct.edu>
2019-06-05 08:42:07 -0400

    explore properties of mongodb document ids

--- node/task-manager/mongodb.js
+++ node/task-manager/mongodb.js
@@ -1,13 +1,14 @@
 // CRUD operations with MongoDB
 
-
 const {MongoClient, ObjectID} = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
 const id = new ObjectID()
-console.log(id)
+console.log(id.id.length)
+console.log(id.toHexString().length)
+console.log(id.getTimestamp())
 
 MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
   if (error) {
@@ -16,15 +17,17 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
 
   const db = client.db(databaseName)
 
-  //db.collection('users').insertOne({
-  //  name: 'Robby',
-  //  age: 22
-  //}, (error, result) => {
-  //  if (error) {
-  //    return console.log('Unable to insert user')
-  //  }
-  //console.log(result.ops)
-  //
+  db.collection('users').insertOne({
+    _id: id,
+    name: 'Ribby Reptar',
+    age: 220
+  }, (error, result) => {
+    if (error) {
+      return console.log('Unable to insert user')
+    }
+  console.log(result.ops)
+})
+
   //db.collection('users').insertMany([
   //  {
   //    name: 'Jim',
