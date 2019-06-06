6a84c6590bac5ae02618f9d4700215e8d20c0b85
Robert Bergers <bergersr@my.easternct.edu>
2019-06-06 15:22:08 -0400

    create user model and store a user to database

--- node/task-manager/src/db/mongoose.js
+++ node/task-manager/src/db/mongoose.js
@@ -4,3 +4,23 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useNewUrlParser: true,
   useCreateIndex: true
 })
+
+const User = mongoose.model('User', {
+  name: {
+    type: String
+  },
+  age: {
+    type: Number
+  }
+})
+
+const me = new User({
+  name: 'Robby',
+  age: 22
+})
+
+me.save().then((result) => {
+  console.log(me)
+}).catch((error) => {
+  console.log('Error: ', error)
+})
