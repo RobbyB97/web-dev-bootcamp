2c3eff3ab936aa88c613cd8f476c53d07e2752cd
Robert Bergers <bergersr@my.easternct.edu>
2019-06-07 16:15:09 -0400

    learn better method of promise chaining

--- node/playground/9-promise-chaining.js
+++ node/playground/9-promise-chaining.js
@@ -6,14 +6,23 @@ const add= (a, b) => {
   })
 }
 
-add(1, 2).then((sum) => {
-  console.log(sum)
+//add(1, 2).then((sum) => {
+//  console.log(sum)
+//
+//  add(sum, 5).then((sum2) => {
+//    console.log(sum2)
+//  }).catch((e2) => {
+//    console.log(e2)
+//  })
+//}).catch((e) => {
+//  console.log(e)
+//})
 
-  add(sum, 5).then((sum2) => {
-    console.log(sum2)
-  }).catch((e2) => {
-    console.log(e2)
-  })
+add(1, 1).then((sum) => {
+  console.log(sum)
+  return add(sum, 4)
+}).then((sum2) => {
+  console.log(sum2)
 }).catch((e) => {
   console.log(e)
 })
