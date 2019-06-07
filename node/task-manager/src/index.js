075d17b39b550d49e846572f69685884999231b1
Robert Bergers <bergersr@my.easternct.edu>
2019-06-07 15:09:00 -0400

    store user sent by post request

--- node/task-manager/src/index.js
+++ node/task-manager/src/index.js
@@ -1,7 +1,24 @@
 const express = require('express')
+require('./db/mongoose')
+const User = require('./models/user.js')
+const Task = require('./models/task.js')
+
 
 const app = express()
 const port = process.env.PORT || 3000
+
+app.use(express.json())
+
+app.post('/users', (req, res) => {
+  const user = new User(req.body)
+
+  user.save().then(() => {
+    res.send(user)
+    console.log(user)
+  }).catch(() => {
+    console.log('Error!')
+  })
+})
 
 app.listen(port, () => {
   console.log('Server is up on port ', port)
