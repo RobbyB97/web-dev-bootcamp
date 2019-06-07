d5c6295f270c87d45df31e0ca1c0394fe5a9573d
Robert Bergers <bergersr@my.easternct.edu>
2019-06-07 14:58:51 -0400

    export models

--- node/task-manager/src/models/task.js
+++ node/task-manager/src/models/task.js
@@ -12,3 +12,6 @@ const Task = mongoose.model('Task', {
     default: false
   }
 })
+
+
+module.exports = Task
--- node/task-manager/src/models/user.js
+++ node/task-manager/src/models/user.js
@@ -44,3 +44,6 @@ const User = mongoose.model('User', {
     }
   }
 })
+
+
+module.exports = User
