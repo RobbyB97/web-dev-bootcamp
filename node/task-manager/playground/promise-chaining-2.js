c58279ae0bff73d0d5aca9ae76354f54f53455bc
Robert Bergers <bergersr@my.easternct.edu>
2019-06-10 14:35:50 -0400

    remove old promise chaining method

--- node/task-manager/playground/promise-chaining-2.js
+++ node/task-manager/playground/promise-chaining-2.js
@@ -1,15 +1,6 @@
 require('../src/db/mongoose.js')
 const Task = require('../src/models/task')
 
-// Task id: 5cf96a999c7b695427cfd1a5
-Task.findByIdAndDelete('5cf96ad2ca97df5448d7fac1').then(() => {
-  console.log('Task deleted')
-  return Task.countDocuments({completed: false})
-}).then((result) => {
-  console.log(result)
-}).catch((e) => {
-  console.log(e)
-})
 
 const deleteTaskAndCount = async (_id) => {
   const task = await Task.findByIdAndDelete(_id)
