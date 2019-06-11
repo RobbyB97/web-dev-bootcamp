4d875dafcc3761c8660524be7a73759fd00e0b3b
Robert Bergers <bergersr@my.easternct.edu>
2019-06-11 16:55:30 -0400

    put task related routes in dedicated file

---
+++ node/task-manager/src/routers/task.js
@@ -0,0 +1,75 @@
+const Task = require('../models/task')
+const express = require('express')
+const router = new express.Router()
+
+
+// CRUD tasks
+router.post('/tasks', async (req, res) => {    // Create new Task
+  const task = new Task(req.body)
+
+  try {
+    await task.save()
+    res.status(201).send(task)
+  } catch (e) {
+    res.status(400).send(e)
+  }
+})
+
+router.get('/tasks', async (req, res) => {     // Read Tasks collection
+
+  try {
+    const tasks = await Task.find({})
+    res.send(tasks)
+  } catch(e) {
+    res.status(500).send()
+  }
+})
+
+router.get('/tasks/:id', async (req, res) => {   // Find Task by ID
+  const _id = req.params.id
+
+  try {
+    const task = await Task.findById(_id)
+    if (!task) {
+      return res.status(404).send()
+    }
+    res.send(task)
+  } catch(e) {
+    res.status(500).send()
+  }
+})
+
+router.patch('/tasks/:id', async (req, res) => {   // Update Task by ID
+  const updates = Object.keys(req.body)
+  const allowedUpdates = ['task', 'completed']
+  const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+  if (!isValidOperation) {
+    return res.status(400).send({error: 'Invalid update!'})
+  }
+
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, {task: req.body.task}, {new: true, runValidators: true})
+    if (!task) {
+      return res.status(404).send()
+    }
+    res.send(task)
+  } catch(e) {
+    res.status(400).send(e)
+  }
+})
+
+router.delete('/tasks/:id', async (req, res) => {    // Delete Task by ID
+
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id)
+    if (!task) {
+      res.status(404).send()
+    }
+    res.send(task)
+  } catch(e) {
+    res.status(500).send()
+  }
+})
+
+module.exports = router
