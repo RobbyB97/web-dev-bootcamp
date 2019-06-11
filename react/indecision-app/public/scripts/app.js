e159f6cd964d03671eef23cbe96af806a4a1e381
Robert Bergers <bergersr@my.easternct.edu>
2019-06-11 07:27:59 -0400

    generate js from jsx with babel

--- react/indecision-app/public/scripts/app.js
+++ react/indecision-app/public/scripts/app.js
@@ -1,1 +1,12 @@
-console.log('app.js is running')
+'use strict';
+
+console.log('app.js is running');
+
+var template = React.createElement(
+  'p',
+  null,
+  'JSX P tag'
+);
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
