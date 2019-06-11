0af02eda1a5721804732324a40469cd9e3984400
Robert Bergers <bergersr@my.easternct.edu>
2019-06-11 16:11:12 -0400

    fix update user route and add update task route

--- node/task-manager/src/index.js
+++ node/task-manager/src/index.js
@@ -47,9 +47,16 @@ app.get('/users/:id', async (req, res) => {
 })
 
 app.patch('/users/:id', async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ['name', 'email', 'password', 'age'];
+  const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+  if (!isValidOperation) {
+    return res.status(400).send({error: 'Invalid update!'})
+  }
 
   try {
-    const user = await User.findByIdAndUpdate(req.params.id, {name: req.body}, {new: true, runValidators: true})
+    const user = await User.findByIdAndUpdate(req.params.id, {name: req.body.name}, {new: true, runValidators: true})
     if (!user) {
       return res.status(404).send()
     }
@@ -94,6 +101,27 @@ app.get('/tasks/:id', async (req, res) => {
   } catch(e) {
     res.status(500).send()
   }
+})
+
+app.patch('/tasks/:id', async (req, res) => {
+  const updates = Object.keys(req.body)
+  const allowedUpdates = ['task', 'completed']
+  const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+  if (!isValidOperation) {
+    return res.status(400).send({error: 'Invalid update!'})
+  }
+
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, {task: req.body.task}, {new: true, runValidators: true})
+    if (!task) {
+      return res.status(404).send()
+    }
+    res.send(task)
+  } catch(e) {
+    res.status(400).send(e)
+  }
+
 })
 
 
