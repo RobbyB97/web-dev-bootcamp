cb52138581def67081f5ea16d7c8d24e32f41291
Robert Bergers <bergersr@my.easternct.edu>
2019-06-12 08:58:42 -0400

    add login route

--- node/task-manager/src/routers/user.js
+++ node/task-manager/src/routers/user.js
@@ -15,6 +15,15 @@ router.post('/users', async (req, res) => {    //Create user
   }
 })
 
+router.post('/users/login', async (req, res) => {   // Login
+  try {
+    const user = await User.findByCredentials(req.body.email, req.body.password)
+    res.send(user)
+  } catch(e) {
+    res.status(400).send()
+  }
+})
+
 router.get('/users', async (req, res) => {     // Read Users collection
 
   try {
