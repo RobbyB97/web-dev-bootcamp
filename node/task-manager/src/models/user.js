bef873d0fce8f3c684c15be432f16e370655c489
Robert Bergers <bergersr@my.easternct.edu>
2019-06-12 10:23:52 -0400

    store valid tokens in user's document

--- node/task-manager/src/models/user.js
+++ node/task-manager/src/models/user.js
@@ -45,13 +45,23 @@ const userSchema = new mongoose.Schema({
         throw new Error('Password cannot contain password!')
       }
     }
-  }
+  },
+  tokens: [{
+    token: {
+      type: String,
+      required: true
+    }
+  }]
 })
 
 // Auth token generator (methods = 'instance methods', accessible on instances)
 userSchema.methods.generateAuthToken = async function() {
   const user = this
   const token = jwt.sign({_id: user._id.toString()}, 'thisisasecret')
+
+  user.tokens = user.tokens.concat({token})
+  await user.save()
+
   return token
 }
 
