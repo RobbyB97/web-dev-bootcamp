4a770095334073ce7d20e0c6587387b024a4e084
Robert Bergers <bergersr@my.easternct.edu>
2019-06-15 11:11:51 -0400

    authenticate endpoints for creating and reading tasks

--- node/task-manager/src/routers/task.js
+++ node/task-manager/src/routers/task.js
@@ -19,27 +19,27 @@ router.post('/tasks', auth, async (req, res) => {    // Create new Task
   }
 })
 
-router.get('/tasks', async (req, res) => {     // Read Tasks collection
+router.get('/tasks', auth, async (req, res) => {     // Read Tasks collection
 
   try {
-    const tasks = await Task.find({})
+    const tasks = await Task.find({user: req.user._id})
     res.send(tasks)
   } catch(e) {
     res.status(500).send()
   }
 })
 
-router.get('/tasks/:id', async (req, res) => {   // Find Task by ID
+router.get('/tasks/:id', auth, async (req, res) => {   // Find Task by ID
   const _id = req.params.id
 
   try {
-    const task = await Task.findById(_id)
+    const task = await Task.findOne({_id, user: req.user._id})
     if (!task) {
       return res.status(404).send()
     }
     res.send(task)
   } catch(e) {
-    res.status(500).send()
+    res.status(500).send(e)
   }
 })
 
