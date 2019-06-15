cc1d7cd90807c420c9e1c09c116bdb1a60c0bc8a
Robert Bergers <bergersr@my.easternct.edu>
2019-06-15 10:40:19 -0400

    add user field to task model

--- node/task-manager/src/models/task.js
+++ node/task-manager/src/models/task.js
@@ -10,6 +10,10 @@ const Task = mongoose.model('Task', {
   completed: {
     type: Boolean,
     default: false
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true
   }
 })
 
