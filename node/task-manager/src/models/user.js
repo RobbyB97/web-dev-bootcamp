ea1b2bf067d49ce7f53e8eb7be51bd1e9d373ec3
Robert Bergers <bergersr@my.easternct.edu>
2019-06-17 16:39:57 -0400

    add middleware function to delete users tasks when user is deleted

--- node/task-manager/src/models/user.js
+++ node/task-manager/src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const Task = require('./task')
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -109,6 +110,13 @@ userSchema.pre('save', async function (next) {
     user.password = await bcrypt.hash(user.password, 8)
   }
 
+  next()
+})
+
+// Delete all user data when user gets deleted
+userSchema.pre('remove', async function (next) {
+  const user = this
+  await Task.deleteMany({user: user._id})
   next()
 })
 
