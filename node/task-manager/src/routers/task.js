17b04753245e18ffd3e55e7c5ab0014cf8baaa3e
Robert Bergers <bergersr@my.easternct.edu>
2019-06-19 17:27:26 -0400

    add pagination capability to GET /tasks

--- node/task-manager/src/routers/task.js
+++ node/task-manager/src/routers/task.js
@@ -29,7 +29,11 @@ router.get('/tasks', auth, async (req, res) => {     // Read Tasks collection
   try {
     await req.user.populate({
       path: 'tasks',
-      match
+      match,
+      options: {
+        limit: parseInt(req.query.limit),
+        skip: parseInt(req.query.skip)
+      }
     }).execPopulate()
     res.send(req.user.tasks)
   } catch(e) {
