14af78a8e3c73d7c1e0ef28a73b45386989c511b
Robert Bergers <bergersr@my.easternct.edu>
2019-06-20 15:11:01 -0400

    remove testing route and function

--- node/task-manager/src/index.js
+++ node/task-manager/src/index.js
@@ -7,15 +7,6 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-const multer = require('multer')
-const upload = multer({
-  dest: 'images'
-})
-app.post('/upload', upload.single('upload'), (req, res) => {
-  res.send()
-})
-
-
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
@@ -24,19 +15,3 @@ app.use(taskRouter)
 app.listen(port, () => {
   console.log('Server is up on port ', port)
 })
-
-const Task = require('./models/task')
-const User = require('./models/user')
-
-const main = async () => {
-  //const task = await Task.findById('5d0504177487962608abce38')
-  //await task.populate('user').execPopulate()
-  //console.log(task)
-
-  const user = await User.findById('5d05032329084b2484a89d2f')
-  await user.populate('tasks').execPopulate()
-  console.log(user.tasks)
-}
-
-
-main()
