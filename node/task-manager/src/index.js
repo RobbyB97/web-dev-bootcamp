5af14bed4005c8bc46246471a814bf7a323976ef
Robert Bergers <bergersr@my.easternct.edu>
2019-06-21 13:16:52 -0400

    learn proper error handling with multer

--- node/task-manager/src/index.js
+++ node/task-manager/src/index.js
@@ -11,6 +11,34 @@ app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
+// Learn error handling
+const multer = require('multer')
+
+const upload = multer({
+  dest: 'images',
+  limits: {
+    fileSize: 1000000,
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(doc|docx)$/)) {
+      return cb(new Error('Please upload .doc or .docx'))
+    }
+
+    cb(undefined, true)
+  }
+})
+
+const errorMidware = (req, res, next) => {
+  throw new Error('From errorMidware')
+}
+
+// testing route
+app.post('/upload', upload.single('upload'), (req, res) => {
+  res.send()
+}, (error, req, res, next) => {
+  res.status(400).send({error: error.message})
+})
+
 // Run server
 app.listen(port, () => {
   console.log('Server is up on port ', port)
