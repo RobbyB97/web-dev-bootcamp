400dec90f154700291296ca539c3297d5fd226cf
Robert Bergers <bergersr@my.easternct.edu>
2019-06-23 12:09:48 -0400

    send first email through sendgrid

--- node/task-manager/src/emails/account.js
+++ node/task-manager/src/emails/account.js
@@ -1,3 +1,13 @@
 const fs = require('fs')
+const sgMail = require('@sendgrid/mail')
 
-const sendgridkey = fs.readFileSync('./sendgridkey.txt').toString().replace(' ', '')
+// Set API key
+const sendgridkey = fs.readFileSync('src/emails/sendgrid2key.txt').toString().replace(' ', '').replace('\n', '')
+sgMail.setApiKey(sendgridkey)
+
+sgMail.send({
+  to: 'bergersr@my.easternct.edu',
+  from: 'bergersr@my.easternct.edu',
+  subject: 'This is a test',
+  text: 'Please just work'
+})
