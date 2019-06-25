c6b7d8aeca9df6a96702ab833e8af687e4d33070
Robert Bergers <bergersr@my.easternct.edu>
2019-06-25 15:24:04 -0400

    get server email from environment variable

--- node/task-manager/src/emails/account.js
+++ node/task-manager/src/emails/account.js
@@ -6,7 +6,7 @@ sgMail.setApiKey(process.env.SENDGRID_KEY)
 const welcome = (email, name) => {
   sgMail.send({
     to: email,
-    from: 'bergersr@my.easternct.edu',
+    from: process.env.SERVER_EMAIL,
     subject: 'Thanks for joining the task app!',
     text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
   })
@@ -15,7 +15,7 @@ const welcome = (email, name) => {
 const goodbye = (email, name) => {
   sgMail.send({
     to: email,
-    from: 'bergersr@my.easternct.edu',
+    from: process.env.SERVER_EMAIL,
     subject: 'One more thing, before you go.',
     text: `We're sorry to see you leave the task app, ${name}. We would appreciate if you let us know why you decided to leave, so we can improve our service. Thank you for your time.`
   })
