b55d859158b86b5e6d24ce242d6a65d11a83d9aa
RobbyB97 <bergersr@my.easternct.edu>
2019-06-27 17:47:24 -0400

    test POST /users/login route

--- node/task-manager/tests/user.test.js
+++ node/task-manager/tests/user.test.js
@@ -1,11 +1,17 @@
 const request = require('supertest')
 const app = require('../src/app')
+const User = require('../src/models/user')
 
-beforeEach(() => {
+const testUserOne = {
+  name: 'Rob',
+  email: 'rob@example.com',
+  password: 'passeeword1'
+}
+
+beforeEach(async () => {
   await User.deleteMany() // Delete User collection
+  await new User(testUserOne).save()
 })
-
-
 
 test('Should sign up a new user', async () => {
   await request(app).post('/users').send({
@@ -13,4 +19,11 @@ test('Should sign up a new user', async () => {
     email: 'bergersr@my.easternct.edu',
     password: 'passport1'
   }).expect(201)
+})
+
+test('Should log in existing user', async () => {
+  await request(app).post('/users/login').send({
+    email: testUserOne.email,
+    password: testUserOne.password
+  }).expect(200)
 })
