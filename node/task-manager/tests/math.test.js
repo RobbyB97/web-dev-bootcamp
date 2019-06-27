fb30a547debf6533104911cb227c586260af7199
Robert Bergers <me@robertbergers.com>
2019-06-26 18:04:02 -0700

    more test cases for basic functions

--- node/task-manager/tests/math.test.js
+++ node/task-manager/tests/math.test.js
@@ -1,10 +1,21 @@
-const {calculateTip} = require('../src/math')
+const {calculateTip, fahrenheitToCelsius, celsiusToFahrenheit} = require('../src/math')
 
 test('Should calculate total plus tip', () => {
     const total = calculateTip(10, 0.3)
     expect(total).toBe(13)
-
-    //if (total !== 13) {
-    //    throw new Error('Total tip should be 13. Got ' + total)
-    //}
 }) 
+
+test('Should calculate total using default tip', () => {
+    const total = calculateTip(20)
+    expect(total).toBe(25)
+})
+
+test('Should convert 32F to 0C', () => {
+    const temp = fahrenheitToCelsius(32)
+    expect(temp).toBe(0)
+})
+
+test('Should convert 0C to 32F', () => {
+    const temp = celsiusToFahrenheit(0)
+    expect(temp).toBe(32)
+})
