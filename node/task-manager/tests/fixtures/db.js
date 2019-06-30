4c67f8f247bcd17a27be7bda5d092d7c4e9fc46c
Robert Bergers <me@robertbergers.com>
2019-06-30 07:27:43 -0700

    add second test user

--- node/task-manager/tests/fixtures/db.js
+++ node/task-manager/tests/fixtures/db.js
@@ -1,12 +1,24 @@
 const mongoose = require('mongoose')
 const jwt = require('jsonwebtoken')
 const User = require('../../src/models/user')
+const Task = require('../../src/models/task')
 
 const userOneId = new mongoose.Types.ObjectId()
 const testUserOne = {
     _id: userOneId,
     name: 'Rob',
     email: 'rob@example.com',
+    password: 'passeeword1',
+    tokens: [{
+        token: jwt.sign({_id: userOneId}, process.env.JWT_SECRET)
+    }]
+}
+
+const userTwoId = new mongoose.Types.ObjectId()
+const testUserTwo = {
+    _id: userTwoId,
+    name: 'Robby',
+    email: 'robby@example.com',
     password: 'passeeword1',
     tokens: [{
         token: jwt.sign({_id: userOneId}, process.env.JWT_SECRET)
