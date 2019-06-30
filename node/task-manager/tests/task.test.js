6df6dfd75133a1e5a962f734031e5464a2a20745
RobbyB97 <bergersr@my.easternct.edu>
2019-06-30 18:54:14 -0400

    fix indentation

--- node/task-manager/tests/task.test.js
+++ node/task-manager/tests/task.test.js
@@ -7,27 +7,27 @@ beforeEach(setupDatabase)
 
 // POST /tasks
 test('Should create task for user', async () => {
-    const response = await request(app)
-        .post('/tasks')
-        .set('Authorization', `Bearer ${testUserOne.tokens[0].token}`)
-        .send({
-            task: 'From test'
-        })
-        .expect(201)
+  const response = await request(app)
+    .post('/tasks')
+    .set('Authorization', `Bearer ${testUserOne.tokens[0].token}`)
+    .send({
+      task: 'From test'
+    })
+    .expect(201)
 
-    const task = await(Task.findById(response.body._id))
-    expect(task).not.toBeNull()     // Ensure task found
-    expect(task.completed).toEqual(false)   // Ensure proper default value
+  const task = await(Task.findById(response.body._id))
+  expect(task).not.toBeNull()     // Ensure task found
+  expect(task.completed).toEqual(false)   // Ensure proper default value
 })
 
 // GET /tasks
 test('Should return tasks for a user', async () => {
-    const response = await request(app)
-        .get('/tasks')
-        .set('Authorization', `Bearer ${testUserOne.tokens[0].token}`)
-        .send()
-        .expect(200)
-    expect(response.body.length).toBe(2)
+  const response = await request(app)
+    .get('/tasks')
+    .set('Authorization', `Bearer ${testUserOne.tokens[0].token}`)
+    .send()
+    .expect(200)
+  expect(response.body.length).toBe(2)
 })
 
 // DELETE /tasks/:id
