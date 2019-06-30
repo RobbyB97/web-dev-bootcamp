c442194f19bc7559d26af3ace5185c2639a132f2
Robert Bergers <me@robertbergers.com>
2019-06-30 07:16:20 -0700

    imports

--- node/task-manager/tests/task.test.js
+++ node/task-manager/tests/task.test.js
@@ -1,5 +1,9 @@
 const request = require('supertest')
+const app = require('../src/app')
 const Task = require('../src/models/task')
+const {userOneId, testUserOne, setupDatabase} = require('./fixtures/db')
+
+beforeEach(setupDatabase)
 
 test('Should create task for user', async () => {
 
