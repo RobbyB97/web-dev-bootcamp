4a89cb0699a787d679163fd9ddf33f96ffa8906f
RobbyB97 <bergersr@my.easternct.edu>
2019-07-20 17:58:50 -0400

    add username to text messages on client side

--- node/chat-app/public/js/chat.js
+++ node/chat-app/public/js/chat.js
@@ -25,6 +25,7 @@ socket.on('message', message => { // System message
 socket.on('emitMessage', message => { // Chat app message
   console.log(message)
   const html = Mustache.render(messageTemplate, {
+    username: message.username,
     message: message.text,
     createdAt: moment(message.createdAt).format('h:mm a')
   })
