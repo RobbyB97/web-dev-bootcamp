ee45cd652a1e5e98f3647948a37d0df49e518579
RobbyB97 <bergersr@my.easternct.edu>
2019-07-20 17:45:34 -0400

    update generateLocationMessage to take username as an argument

--- node/chat-app/src/utils/messages.js
+++ node/chat-app/src/utils/messages.js
@@ -4,8 +4,9 @@ const generateMessage = (text) => {
     createdAt: new Date().getTime()
   }
 }
-const generateLocationMessage = (url) => {
+const generateLocationMessage = (username, url) => {
   return {
+    username,
     url,
     createdAt: new Date().getTime()
   }
