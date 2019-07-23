3270facb944c4a95f2d169335e8f1566f85089ed
RobbyB97 <bergersr@my.easternct.edu>
2019-07-23 18:58:10 -0400

    put app copy in playground for reference

---
+++ react/indecision-app/playground/counter-example.js
@@ -0,0 +1,82 @@
+console.log('app.js is running')
+
+
+// Variables
+const app = {
+  title: 'Indecision App',
+  subtitle: 'Some info',
+  options: ['One', 'Two']
+}
+const user = {
+  name: 'Rob',
+  age: 19,
+  location: 'Willimantic'
+}
+
+const $appRoot = document.getElementById('app')
+
+let count = 0
+
+
+// Functions
+const getLocation = location => {
+  if (location) {
+    return <p> Location: {location} </p>
+  }
+}
+
+const addOne = () => {
+  count++
+  renderCounterApp()
+}
+
+const minusOne = () => {
+  count--
+  renderCounterApp()
+}
+
+const resetCount = () => {
+  count = 0
+  renderCounterApp()
+}
+
+
+// Templates
+const template = (
+  <div>
+    <h1>{app.title}</h1>
+    {app.subtitle && <p>{app.subtitle}</p>}
+    <p>{(app.options.length > 0) ? "Here are your options" : "No options"}</p>
+    <ol>
+      <li> Item one </li>
+      <li> Item two </li>
+    </ol>
+  </div>
+)
+
+const templateTwo = (
+  <div>
+    <h1> {user.name ? user.name : 'Anonymous'} </h1>
+    {(user.age && user.age >= 18) && <p> Age: {user.age} </p>}
+    {getLocation(user.location)}
+  </div>
+)
+
+
+// Render app
+const renderCounterApp = () => {
+  const countTemplate = (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={addOne}>+1</button>
+      <button onClick={minusOne}>-1</button>
+      <button onClick={resetCount}>Reset</button>
+    </div>
+  )
+
+  ReactDOM.render(countTemplate, $appRoot)
+}
+
+
+// Render App
+renderCounterApp()
