0349703cc498e24639b6122ed33388e2a243cfe3
RobbyB97 <bergersr@my.easternct.edu>
2019-07-23 19:45:17 -0400

    refactor: render template through a function

--- react/indecision-app/public/scripts/app.js
+++ react/indecision-app/public/scripts/app.js
@@ -41,49 +41,60 @@ var onFormSubmit = function onFormSubmit(e) {
 };
 
 // Templates
-var template = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
-    null,
-    app.title
-  ),
-  app.subtitle && React.createElement(
-    'p',
-    null,
-    app.subtitle
-  ),
-  React.createElement(
-    'p',
-    null,
-    app.options.length > 0 ? "Here are your options" : "No options"
-  ),
-  React.createElement(
-    'ol',
+
+
+// Render Functions
+var renderCounterApp = function renderCounterApp() {
+  var template = React.createElement(
+    'div',
     null,
     React.createElement(
-      'li',
+      'h1',
       null,
-      ' Item one '
+      app.title
+    ),
+    app.subtitle && React.createElement(
+      'p',
+      null,
+      app.subtitle
     ),
     React.createElement(
-      'li',
+      'p',
       null,
-      ' Item two '
-    )
-  ),
-  React.createElement(
-    'form',
-    { onSubmit: onFormSubmit },
-    React.createElement('input', { type: 'text', name: 'option' }),
+      app.options.length > 0 ? "Here are your options" : "No options"
+    ),
     React.createElement(
-      'button',
+      'p',
       null,
-      'Add Option'
+      app.options.length
+    ),
+    React.createElement(
+      'ol',
+      null,
+      React.createElement(
+        'li',
+        null,
+        ' Item one '
+      ),
+      React.createElement(
+        'li',
+        null,
+        ' Item two '
+      )
+    ),
+    React.createElement(
+      'form',
+      { onSubmit: onFormSubmit },
+      React.createElement('input', { type: 'text', name: 'option' }),
+      React.createElement(
+        'button',
+        null,
+        'Add Option'
+      )
     )
-  )
-);
+  );
+  ReactDOM.render(template, $appRoot);
+};
 
-// Render App
-ReactDOM.render(template, $appRoot);
+// Script
+renderCounterApp();
