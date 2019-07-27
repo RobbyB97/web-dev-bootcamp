04469f49ab8867090eb86b4c976f5be35680c75f
RobbyB97 <bergersr@my.easternct.edu>
2019-07-27 15:53:16 -0400

    add greeting method to Person class

--- react/indecision-app/playground/3-es6-classes.js
+++ react/indecision-app/playground/3-es6-classes.js
@@ -1,12 +1,15 @@
 
 class Person {
-  constructor(name) {
+  constructor(name='This one') {
     this.name = name
+  }
+  getGreeting() {
+    return `Hello ${this.name}!`
   }
 }
 
 const me = new Person('Robby Bergers')
-console.log(me)
+console.log(me.getGreeting())
 
 const other = new Person()
-console.log(other)
+console.log(other.getGreeting())
