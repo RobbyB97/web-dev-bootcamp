0187011b5ee5406ed77e768d34a731a0177aa835
RobbyB97 <bergersr@my.easternct.edu>
2019-07-27 15:40:09 -0400

    render indecision app

--- react/indecision-app/public/scripts/app.js
+++ react/indecision-app/public/scripts/app.js
@@ -1,20 +1,59 @@
 'use strict';
 
-console.log('build-it-visible.js is running..');
+console.log('app.js is running');
+
+// Variables
+var app = {
+  title: 'Indecision App',
+  subtitle: 'Some info',
+  options: []
+};
+var user = {
+  name: 'Rob',
+  age: 19,
+  location: 'Willimantic'
+};
 
 var $appRoot = document.getElementById('app');
 
-var visible = 0;
-
-var onBtnClick = function onBtnClick() {
-  if (visible === 0) {
-    visible = 1;
-  } else {
-    visible = 0;
+// Functions
+var getLocation = function getLocation(location) {
+  if (location) {
+    return React.createElement(
+      'p',
+      null,
+      ' Location: ',
+      location,
+      ' '
+    );
   }
+};
+
+var onFormSubmit = function onFormSubmit(e) {
+  e.preventDefault();
+  var option = e.target.elements.option.value;
+
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = '';
+    console.log(app.options);
+  }
+
   render();
 };
 
+var onMakeDecision = function onMakeDecision() {
+  var randomNum = Math.floor(Math.random() * app.options.length);
+  var option = app.options[randomNum];
+  console.log(option);
+};
+
+var wipeOptions = function wipeOptions() {
+  app.options = [];
+  render();
+};
+
+// Render Functions
 var render = function render() {
   var template = React.createElement(
     'div',
@@ -22,20 +61,52 @@ var render = function render() {
     React.createElement(
       'h1',
       null,
-      'Visibility Toggle'
+      app.title
     ),
-    React.createElement(
-      'button',
-      { onClick: onBtnClick },
-      visible ? 'Hide details' : 'Show details'
+    app.subtitle && React.createElement(
+      'p',
+      null,
+      app.subtitle
     ),
     React.createElement(
       'p',
       null,
-      visible ? "" : "Here are some more details m8"
+      app.options.length > 0 ? "Here are your options" : "No options"
+    ),
+    React.createElement(
+      'button',
+      { disabled: app.options.length === 0, onClick: onMakeDecision },
+      'What should I do?'
+    ),
+    React.createElement(
+      'button',
+      { onClick: wipeOptions },
+      'Delete Options'
+    ),
+    React.createElement(
+      'ol',
+      null,
+      app.options.map(function (option) {
+        return React.createElement(
+          'li',
+          { key: option },
+          option
+        );
+      })
+    ),
+    React.createElement(
+      'form',
+      { onSubmit: onFormSubmit },
+      React.createElement('input', { type: 'text', name: 'option' }),
+      React.createElement(
+        'button',
+        null,
+        'Add Option'
+      )
     )
   );
   ReactDOM.render(template, $appRoot);
 };
 
+// Script
 render();
