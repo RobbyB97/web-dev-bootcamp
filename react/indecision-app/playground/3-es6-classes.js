c3b219863678e15da586df13589a601c30ec36c3
RobbyB97 <bergersr@my.easternct.edu>
2019-07-27 15:49:07 -0400

    create Person class

---
+++ react/indecision-app/playground/3-es6-classes.js
@@ -0,0 +1,12 @@
+
+class Person {
+  constructor(name) {
+    this.name = name
+  }
+}
+
+const me = new Person('Robby Bergers')
+console.log(me)
+
+const other = new Person()
+console.log(other)
--- react/indecision-app/public/scripts/app.js
+++ react/indecision-app/public/scripts/app.js
@@ -1,112 +1,15 @@
 'use strict';
 
-console.log('app.js is running');
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-// Variables
-var app = {
-  title: 'Indecision App',
-  subtitle: 'Some info',
-  options: []
-};
-var user = {
-  name: 'Rob',
-  age: 19,
-  location: 'Willimantic'
+var Person = function Person(name) {
+  _classCallCheck(this, Person);
+
+  this.name = name;
 };
 
-var $appRoot = document.getElementById('app');
+var me = new Person('Robby Bergers');
+console.log(me);
 
-// Functions
-var getLocation = function getLocation(location) {
-  if (location) {
-    return React.createElement(
-      'p',
-      null,
-      ' Location: ',
-      location,
-      ' '
-    );
-  }
-};
-
-var onFormSubmit = function onFormSubmit(e) {
-  e.preventDefault();
-  var option = e.target.elements.option.value;
-
-  if (option) {
-    app.options.push(option);
-    e.target.elements.option.value = '';
-    console.log(app.options);
-  }
-
-  render();
-};
-
-var onMakeDecision = function onMakeDecision() {
-  var randomNum = Math.floor(Math.random() * app.options.length);
-  var option = app.options[randomNum];
-  console.log(option);
-};
-
-var wipeOptions = function wipeOptions() {
-  app.options = [];
-  render();
-};
-
-// Render Functions
-var render = function render() {
-  var template = React.createElement(
-    'div',
-    null,
-    React.createElement(
-      'h1',
-      null,
-      app.title
-    ),
-    app.subtitle && React.createElement(
-      'p',
-      null,
-      app.subtitle
-    ),
-    React.createElement(
-      'p',
-      null,
-      app.options.length > 0 ? "Here are your options" : "No options"
-    ),
-    React.createElement(
-      'button',
-      { disabled: app.options.length === 0, onClick: onMakeDecision },
-      'What should I do?'
-    ),
-    React.createElement(
-      'button',
-      { onClick: wipeOptions },
-      'Delete Options'
-    ),
-    React.createElement(
-      'ol',
-      null,
-      app.options.map(function (option) {
-        return React.createElement(
-          'li',
-          { key: option },
-          option
-        );
-      })
-    ),
-    React.createElement(
-      'form',
-      { onSubmit: onFormSubmit },
-      React.createElement('input', { type: 'text', name: 'option' }),
-      React.createElement(
-        'button',
-        null,
-        'Add Option'
-      )
-    )
-  );
-  ReactDOM.render(template, $appRoot);
-};
-
-// Script
-render();
+var other = new Person();
+console.log(other);
