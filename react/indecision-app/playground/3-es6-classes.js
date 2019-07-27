7cf7ae4b33e45c351ee41757b4884e3c1aa44c88
RobbyB97 <bergersr@my.easternct.edu>
2019-07-27 16:03:50 -0400

    add Student subclass of Person

--- react/indecision-app/playground/3-es6-classes.js
+++ react/indecision-app/playground/3-es6-classes.js
@@ -12,8 +12,18 @@ class Person {
   }
 }
 
-const me = new Person('Robby Bergers', 22)
+class Student extends Person {
+  constructor(name, age, major="Undecided") {
+    super()
+    this.major = major
+  }
+  getDescription() {
+    return `${this.name} is a ${this.age} year old ${this.major} student.`
+  }
+}
+
+const me = new Student('Robby Bergers', 22, 'Computer Science')
 console.log(me.getDescription())
 
-const other = new Person()
-console.log(other.getDescription())
+const other = new Student()
+console.log(other)
