463d616afff2a6f24a85d12dc96f2bf1899a65dc
RobbyB97 <bergersr@my.easternct.edu>
2019-07-29 17:49:40 -0400

    create and log add function from utils in app

--- react/indecision-app/src/app.js
+++ react/indecision-app/src/app.js
@@ -1,5 +1,6 @@
 //import './utils.js'
-import {square} from './utils.js'
+import {square, add} from './utils.js'
 
 console.log('app.js is running')
 console.log(`Square: ${square(4)}`)
+console.log(`Add: ${add(3, 9)}`)
