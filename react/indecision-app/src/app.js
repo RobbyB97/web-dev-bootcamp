266b4c5d6ed198fc9d900721356eef9361b5df8b
RobbyB97 <bergersr@my.easternct.edu>
2019-07-29 21:00:16 -0400

    remove practice files

--- react/indecision-app/src/app.js
+++ react/indecision-app/src/app.js
@@ -1,13 +1,2 @@
-//import './utils.js'
-import cube, {square, add} from './utils.js'
-import {isAdult, canDrink} from './person.js'
-import validator from 'validator'
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-console.log('app.js is running')
-console.log(`square: ${square(4)}`)
-console.log(`add: ${add(3, 9)}`)
-console.log(`isAdult: ${isAdult(19)}`)
-console.log(`canDrink: ${canDrink(19)}`)
-console.log(`cube: ${cube(4)}`)
--- react/indecision-app/src/utils.js
+++
@@ -1,7 +0,0 @@
-console.log('utils.js is running')
-
-const square = (x) => x * x
-const add = (a, b) => a + b
-const cube = (x) => x * x * x
-
-export {square, add, cube as default}
