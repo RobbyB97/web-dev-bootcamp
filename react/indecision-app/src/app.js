55df6cb56703a66fd254973d6fdbe46cf80a32ea
RobbyB97 <bergersr@my.easternct.edu>
2019-07-30 22:28:14 -0400

    refactor: remove imports for components not referenced

--- react/indecision-app/src/app.js
+++ react/indecision-app/src/app.js
@@ -3,12 +3,7 @@ import ReactDOM from 'react-dom'
 
 
 // Import components
-import AddOption from './components/AddOption'
 import Header from './components/Header'
-import Action from './components/Action'
-import Options from './components/Options'
-import Option from './components/Option'
-import RemoveOptions from './components/RemoveOptions'
 import IndecisionApp from './components/IndecisionApp'
 
 // Default props
