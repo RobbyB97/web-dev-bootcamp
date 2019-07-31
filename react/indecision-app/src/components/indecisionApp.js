eec38fe1c57434c98f1351fa50697e2e1e629679
RobbyB97 <bergersr@my.easternct.edu>
2019-07-30 22:19:51 -0400

    put IndecisionApp component in its own file

---
+++ react/indecision-app/src/components/indecisionApp.js
@@ -0,0 +1,92 @@
+import React from 'react'
+
+import AddOption from './addOption'
+import Header from './header'
+import Action from './action'
+import Options from './options'
+import Option from './option'
+import RemoveOptions from './removeOptions'
+
+export default class IndecisionApp extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleAddOption = this.handleAddOption.bind(this)
+    this.handleDeleteOption = this.handleDeleteOption.bind(this)
+    this.handleDeleteOptions = this.handleDeleteOptions.bind(this)
+    this.handlePick = this.handlePick.bind(this)
+    this.state = {
+      options: props.options
+    }
+  }
+  componentDidMount() { // When component gets created
+    try {
+      const json = localStorage.getItem('options')
+      const options = JSON.parse(json)
+      if (options) {
+        this.setState(() => ({options}))
+      }
+    } catch(e) {
+      console.log('Invalid JSON')
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {  // When component is updated
+    if (prevState.options.length !== this.state.options.length) {
+      const json = JSON.stringify(this.state.options)
+      localStorage.setItem('options', json)
+      console.log('Saving data')
+    }
+  }
+  componentWillUnmount() {
+    console.log('componentWillUnmount')
+  }
+  handlePick() {
+    let num = Math.floor(Math.random() * this.state.options.length)
+    return alert(this.state.options[num])
+  }
+  handleAddOption(option) {
+    if (!option) {
+      return 'Enter valid value to add item'
+    } else if (this.state.options.indexOf(option) > -1) {
+      return 'Option already exists!'
+    }
+    this.setState((prevState) => ({
+      options: prevState.options.concat(option)
+    }))
+  }
+  handleDeleteOption(optionToRemove) {
+    console.log('handleDeleteOption', optionToRemove)
+    this.setState((prevState) => ({
+      options: prevState.options.filter((option) => {
+        return optionToRemove !== option
+      })
+    }))
+  }
+  handleDeleteOptions() {
+    this.setState(() => ({
+      options: []
+    }))
+  }
+  render() {
+    const subtitle = 'Put your life in the hands of a computer!'
+
+    return (
+      <div>
+        <Header subtitle={subtitle} />
+        <Action
+          hasOptions={this.state.options.length === 0}
+          handlePick={this.handlePick}
+        />
+        <Options
+          handleDeleteOption={this.handleDeleteOption}
+          options={this.state.options}
+        />
+        <AddOption
+          handleAddOption={this.handleAddOption}
+        />
+        <RemoveOptions
+          handleDeleteOptions={this.handleDeleteOptions}
+        />
+      </div>
+    )
+  }
+}
