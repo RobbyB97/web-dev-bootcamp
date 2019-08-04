c280d2f9bb16a627e494e881b1d4648c18855d2e
RobbyB97 <bergersr@my.easternct.edu>
2019-08-04 19:29:43 -0400

    error message styles

--- react/indecision-app/src/components/AddOption.js
+++ react/indecision-app/src/components/AddOption.js
@@ -18,7 +18,7 @@ export default class AddOption extends React.Component {
   render() {
     return (
       <div>
-        {this.state.error && <p>{this.state.error}</p>}
+        {this.state.error && <p className="add-option-error">{this.state.error}</p>}
         <form onSubmit={this.onFormSubmit}>
           <input type="text" name="option"/>
           <button className="button">Add Option</button>
