8568e396370d00d6ffec24593737eb8c01c3c7d8
RobbyB97 <bergersr@my.easternct.edu>
2019-08-05 17:58:29 -0400

    add first react router

--- react/expensify-app/src/app.js
+++ react/expensify-app/src/app.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import 'normalize.css/normalize.css'
+import {BrowserRouter, Route} from 'react-router-dom'
 
 import './sass/main.scss'
 
 
 // Variables
 const $appRoot = document.getElementById('app')
+
+
+// Routes
+const routes = (
+  <BrowserRouter>
+
+  </BrowserRouter>
+)
 
 
 ReactDOM.render(<p>This is a boilerplate</p>, $appRoot)
