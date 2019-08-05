4afed9c39e645bc4b9615d4f8161691558159425
RobbyB97 <bergersr@my.easternct.edu>
2019-08-04 21:25:50 -0400

    Create dedicated boilerplate

---
+++ react/boilerplate-v1/src/app.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'normalize.css/normalize.css'
+
+import './sass/main.scss'
+
+
+// Variables
+const $appRoot = document.getElementById('app')
+
+
+ReactDOM.render(<p>This is a boilerplate</p>, $appRoot)
