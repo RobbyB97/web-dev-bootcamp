70299855c32aeeace4420e275e940ca8235afe93
RobbyB97 <bergersr@my.easternct.edu>
2019-08-06 17:17:07 -0400

    delete irrelevant parts to create boilerplate

--- react/portfolio-site/src/components/NotFoundPage.js
+++ react/portfolio-site/src/components/NotFoundPage.js
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom'
 
 const NotFoundPage = () => (
   <div>
-    404 - <Link to="/">Go Home</Link>
+    404 - <NavLink to="/">Go Home</NavLink>
   </div>
 )
 
--- react/portfolio-site/src/routers/AppRouter.js
+++ react/portfolio-site/src/routers/AppRouter.js
@@ -4,10 +4,6 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom'
 
 //Components
 import Header from '../components/Header'
-import ExpenseDashboard from '../components/ExpenseDashboard'
-import ExpenseCreate from '../components/ExpenseCreate'
-import ExpenseHelp from '../components/ExpenseHelp'
-import ExpenseEdit from '../components/ExpenseEdit'
 import NotFoundPage from '../components/NotFoundPage'
 
 // Routes
@@ -16,10 +12,6 @@ const AppRouter = () => (
     <div>
       <Header/>
       <Switch>
-        <Route path="/" component={ExpenseDashboard} exact={true}/>
-        <Route path="/create" component={ExpenseCreate} exact={true}/>
-        <Route path="/edit/:id" component={ExpenseEdit}/>
-        <Route path="/help" component={ExpenseHelp}/>
         <Route component={NotFoundPage}/>
       </Switch>
     </div>
