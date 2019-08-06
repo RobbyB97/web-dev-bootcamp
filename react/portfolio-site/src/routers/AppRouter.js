26bbfbc30568df6cea8c6dbd9e9ea06ebc8ec264
RobbyB97 <bergersr@my.easternct.edu>
2019-08-06 19:37:50 -0400

    import components

--- react/portfolio-site/src/routers/AppRouter.js
+++ react/portfolio-site/src/routers/AppRouter.js
@@ -4,7 +4,10 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom'
 
 //Components
 import Header from '../components/Header'
+import Home from '../components/Home'
 import NotFoundPage from '../components/NotFoundPage'
+import Contact from '../components/Contact'
+import Portfolio from '../components/Portfolio'
 
 // Routes
 const AppRouter = () => (
