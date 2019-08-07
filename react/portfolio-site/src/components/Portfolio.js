6a42e3f7191f9a0400fa3ada6a04a76f6b0ddc1e
RobbyB97 <bergersr@my.easternct.edu>
2019-08-06 20:06:17 -0400

    remove PortfolioItem rendering from Portfolio component

--- react/portfolio-site/src/components/Portfolio.js
+++ react/portfolio-site/src/components/Portfolio.js
@@ -1,26 +1,14 @@
 import React from 'react'
-import {NavLink} from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
-const Portfolio = (props) => {
-  if (props.match.params.id) {
-    return (
-      <div>
-        <h2>Portfolio</h2>
-        <NavLink to="/portfolio/1" exact={true}>Item one</NavLink>
-        <NavLink to="/portfolio/2" exact={true}>Item two</NavLink>
-        <NavLink to="/portfolio/3" exact={true}>Item three</NavLink>
-
-      </div>
-    )
-  }
-
-  return (
-    <div>
-      <h2>Portfolio item: {props.match.params.id}</h2>
-      <p>Here's some information about {props.match.params.id}.</p>
-    </div>
-  )
-}
+const Portfolio = (props) => (
+  <div>
+    <h2>Portfolio</h2>
+    <Link to="/portfolio/1">Item one</Link>
+    <Link to="/portfolio/2">Item two</Link>
+    <Link to="/portfolio/3">Item three</Link>
+  </div>
+)
 
 
 export default Portfolio
