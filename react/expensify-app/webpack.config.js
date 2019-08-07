0d2774fc9288d260f29d59e37df0503ec30817c6
RobbyB97 <bergersr@my.easternct.edu>
2019-08-07 17:55:29 -0400

    configure webpack to run redux file

--- react/expensify-app/webpack.config.js
+++ react/expensify-app/webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path')
 
 
 module.exports = {
-  entry: './src/app.js',
+  entry: './src/playground/1-redux.js',
   output: {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js'
