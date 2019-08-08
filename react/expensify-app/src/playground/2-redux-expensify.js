eb3ec52c6875b1006c8c722ca0e489dc27cac5d7
RobbyB97 <bergersr@my.easternct.edu>
2019-08-07 23:36:46 -0400

    add demoState

--- react/expensify-app/src/playground/2-redux-expensify.js
+++ react/expensify-app/src/playground/2-redux-expensify.js
@@ -1,1 +1,17 @@
-console.log('1-redux-expensify')
+import {createoStore, combineReducers} from 'redux'
+
+const demoState = {
+  expenses: [{
+    id: 'fdshfgndkjgdf',
+    description: 'Rent',
+    note: 'Last rent payment',
+    amount: 54500,
+    createdAt: 0
+  }],
+  filters: {
+    text: 'rent',
+    sortBy: 'amount',
+    startDate: undefined,
+    endDate: undefined
+  }
+}
