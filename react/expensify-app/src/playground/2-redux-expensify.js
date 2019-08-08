964ea1de2f26ccb1b0761e41940913376585f64e
RobbyB97 <bergersr@my.easternct.edu>
2019-08-08 18:57:24 -0400

    create addExpense action generator

--- react/expensify-app/src/playground/2-redux-expensify.js
+++ react/expensify-app/src/playground/2-redux-expensify.js
@@ -1,4 +1,24 @@
 import {createStore, combineReducers} from 'redux'
+import uuid from 'uuid'
+
+
+const addExpense = (
+  {
+    description = '',
+    note='',
+    amount = 0,
+    createdAt = 0
+  } = {}
+) => ({
+  type: 'ADD_EXPENSE',
+  expense: {
+    id: uuid(),
+    description,
+    note,
+    amount,
+    createdAt
+  }
+})
 
 
 // Reducers
