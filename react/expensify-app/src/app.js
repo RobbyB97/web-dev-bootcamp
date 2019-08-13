483b0bac3b5f2f7cbcd3da915423229ecb106efb
RobbyB97 <bergersr@my.easternct.edu>
2019-08-12 20:13:41 -0400

    import store

--- react/expensify-app/src/app.js
+++ react/expensify-app/src/app.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import configureStore from './store/configureStore'
 import 'normalize.css/normalize.css'
 import './sass/main.scss'
 import AppRouter from './routers/AppRouter'
@@ -8,5 +9,8 @@ import AppRouter from './routers/AppRouter'
 // Variables
 const $appRoot = document.getElementById('app')
 
+const store = configureStore()
+
+console.log(store.getState())
 
 ReactDOM.render(<AppRouter />, $appRoot)
