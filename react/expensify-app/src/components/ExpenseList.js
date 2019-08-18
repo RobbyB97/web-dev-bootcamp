cd14ea9566be87d4657c530df4830d3ad032c701
RobbyB97 <bergersr@my.easternct.edu>
2019-08-18 12:18:59 -0400

    test connect function on ExpenseList

--- react/expensify-app/src/components/ExpenseList.js
+++ react/expensify-app/src/components/ExpenseList.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import {connect} from 'react-redux'
 
 const ExpenseList = (props) => (
   <div>
     <h1>Expense List</h1>
+    {props.name}
   </div>
 )
 
-export default ExpenseList
+const ConnectedExpenseList = connect((state) => {
+  return {
+    name: 'Andrew'
+  }
+})(ExpenseList)
+
+export default ConnectedExpenseList
