0fa77a536d419fdf4dea96ef6ed5736731f69376
RobbyB97 <bergersr@my.easternct.edu>
2019-08-18 15:42:46 -0400

    test react updating

--- react/expensify-app/src/app.js
+++ react/expensify-app/src/app.js
@@ -24,6 +24,10 @@ store.dispatch(addExpense({description: 'Gas bill', amount: 400}))
 store.dispatch(setTextFilter('bill'))
 store.dispatch(setTextFilter('water'))
 
+setTimeout(() => {
+    store.dispatch(setTextFilter('rent'))
+}, 3000)
+
 const jsx = (
   <Provider store={store}>
     <AppRouter />
