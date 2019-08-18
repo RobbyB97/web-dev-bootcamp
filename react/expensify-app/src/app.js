7421a93ce32b703452cda01c7ad882b9bba8ac90
RobbyB97 <bergersr@my.easternct.edu>
2019-08-18 12:09:04 -0400

    link Provider to app store

--- react/expensify-app/src/app.js
+++ react/expensify-app/src/app.js
@@ -2,6 +2,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import 'normalize.css/normalize.css'
+import {Provider} from 'react-redux'
 
 // Assets
 import './sass/main.scss'
@@ -16,11 +17,6 @@ const $appRoot = document.getElementById('app')
 
 // Set up redux store
 const store = configureStore()
-store.subscribe(() => {
-  const state = store.getState()
-  const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
-  console.log(visibleExpenses)
-})
 
 console.log(store.getState())
 store.dispatch(addExpense({description: 'Water bill', amount: 450}))
@@ -28,4 +24,10 @@ store.dispatch(addExpense({description: 'Gas bill', amount: 400}))
 store.dispatch(setTextFilter('bill'))
 store.dispatch(setTextFilter('water'))
 
-ReactDOM.render(<AppRouter />, $appRoot)
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+)
+
+ReactDOM.render(jsx, $appRoot)
