5752d5589f51c2c5883e15f0ca829062f93758c2
RobbyB97 <bergersr@my.easternct.edu>
2019-08-18 15:43:00 -0400

    list expenses with ExpenseListItem component

--- react/expensify-app/src/components/ExpenseList.js
+++ react/expensify-app/src/components/ExpenseList.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import {connect} from 'react-redux'
 
+import ExpenseListItem from './ExpenseListItem'
+
 const ExpenseList = (props) => (
   <div>
     <h1>Expense List</h1>
-    {props.expenses.length}
+    {props.expenses.map((expense) => (
+      <ExpenseListItem {...expense} />
+    ))}
   </div>
 )
 
 const mapStateToProps = (state) => {
   return {
-    expenses: state.expenses
+    expenses: state.expenses,
+    filters: state.filters
   }
 }
 
