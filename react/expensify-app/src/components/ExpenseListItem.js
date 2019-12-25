46957bf8bc42db277964b81c939474c0f197a56f
Robert Bergers <me@robertbergers.com>
2019-12-25 09:11:26 -0800

    edit expense button

--- react/expensify-app/src/components/ExpenseListItem.js
+++ react/expensify-app/src/components/ExpenseListItem.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import {connect} from 'react-redux'
 
 import {removeExpense} from '../actions/expenses.js'
@@ -7,9 +8,16 @@ const ExpenseListItem = ({id, dispatch, description, amount, createdAt}) => (
   <div>
     <h6>{description}</h6>
     <p>{amount} | {createdAt}</p>
+    
+    <Link to={`/edit/${id}`}>
+        Edit Expense
+    </Link>
+    
     <button onClick={() => {
       dispatch(removeExpense({id}))
-    }}> Remove Expense </button>
+    }}> 
+        Remove Expense 
+    </button>
 
   </div>
 )
