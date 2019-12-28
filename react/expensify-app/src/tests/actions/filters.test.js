893f9ac6191b2b46c630ad9573b3b0fd65f75013
Robert Bergers <me@robertbergers.com>
2019-12-28 02:33:37 -0800

    set start date and set end date action object tests

--- react/expensify-app/src/tests/actions/filters.test.js
+++ react/expensify-app/src/tests/actions/filters.test.js
@@ -6,10 +6,18 @@ import moment from 'moment'
 
 
 test('Generate set start date action object', () => {
-
+    const action = setStartDate(moment(0))
+    expect(action).toEqual({
+        type: 'SET_START_DATE',
+        startDate: moment(0)
+    })
 })
 
 
 test('Generate set end date action object', () => {
-
+    const action = setEndDate(moment(0))
+    expect(action).toEqual({
+        type: 'SET_END_DATE',
+        endDate: moment(0)
+    })
 })
