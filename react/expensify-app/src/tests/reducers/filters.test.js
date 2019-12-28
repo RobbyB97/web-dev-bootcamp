4f99a3ddf0d4fd26fce29ad64540e19805e9502a
Robert Bergers <me@robertbergers.com>
2019-12-28 06:56:52 -0800

    test filters reducer default state

--- react/expensify-app/src/tests/reducers/filters.test.js
+++ react/expensify-app/src/tests/reducers/filters.test.js
@@ -1,1 +1,14 @@
 import filtersReducer from '../../reducers/filters'
+import moment from 'moment'
+
+
+test('Filters reducer default state', () => {
+    const state = filtersReducer(undefined, {type: '@@INIT'})
+
+    expect(state).toEqual({
+        text: '',
+        sortBy: 'date',
+        startDate: moment().startOf('month'),
+        endDate: moment().endOf('month')
+    })
+})
