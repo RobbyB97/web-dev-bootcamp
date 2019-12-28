b5261326101d323e158031a3678f8a52a692f15c
Robert Bergers <me@robertbergers.com>
2019-12-28 05:53:59 -0800

    test data and filter by text test

--- react/expensify-app/src/tests/selectors/expenses.test.js
+++ react/expensify-app/src/tests/selectors/expenses.test.js
@@ -1,1 +1,36 @@
 import selectExpenses from '../../selectors/expenses'
+
+
+//Test data
+const expenses = [{
+    id: '1',
+    description: 'Gum',
+    note: '',
+    amount: 195,
+    createdAt: 0
+}, {
+    id: '2',
+    description: 'Rent',
+    note: '',
+    amount: 19500,
+    createdAt: -1000
+}, {
+    id: '3',
+    description: 'Credit Card',
+    note: '',
+    amount: 4500,
+    createdAt: 1000
+}]
+
+
+test('Filter by text value', () => {
+    const filters = {
+        text: 'e',
+        sortBy: 'date',
+        startDate: undefined,
+        endDate: undefined
+    }
+    const result = selectExpenses(expenses, filters)
+
+    expect(result).toEqual([expenses[2], expenses[1]])
+})
