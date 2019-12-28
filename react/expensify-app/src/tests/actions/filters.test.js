8aa3c90f45459697ee0409ab3a91bbb213abc266
Robert Bergers <me@robertbergers.com>
2019-12-28 02:31:13 -0800

    filters action object tests initial commit

---
+++ react/expensify-app/src/tests/actions/filters.test.js
@@ -0,0 +1,15 @@
+import {
+    setStartDate, setEndDate, setTextFilter, sortByDate, sortByAmount
+} from '../../actions/filters'
+
+import moment from 'moment'
+
+
+test('Generate set start date action object', () => {
+
+})
+
+
+test('Generate set end date action object', () => {
+
+})
