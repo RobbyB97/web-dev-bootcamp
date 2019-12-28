6efdd62a7fbb1fc0febcb5fb3583568f213e43f1
Robert Bergers <me@robertbergers.com>
2019-12-28 02:38:44 -0800

    set text filter action object tests

--- react/expensify-app/src/tests/actions/filters.test.js
+++ react/expensify-app/src/tests/actions/filters.test.js
@@ -21,3 +21,20 @@ test('Generate set end date action object', () => {
         endDate: moment(0)
     })
 })
+
+
+test('Generate set text filter action object with provided text', () => {
+    const action = setTextFilter('Test')
+    expect(action).toEqual({
+        type: 'SET_TEXT_FILTER',
+        text: 'Test'
+    })
+})
+
+
+test('Generate set text filter action object without provided text', () => {
+    const action = setTextFilter()
+    expect(action).toEqual({
+        type: 'SET_TEXT_FILTER'
+    })
+})
