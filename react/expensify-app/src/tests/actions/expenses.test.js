b18808c290842fb539d0a2d39d36e66d28c77c64
Robert Bergers <me@robertbergers.com>
2019-12-28 02:16:41 -0800

    edit expense test

--- react/expensify-app/src/tests/actions/expenses.test.js
+++ react/expensify-app/src/tests/actions/expenses.test.js
@@ -1,9 +1,18 @@
 import {addExpense, editExpense, removeExpense} from '../../actions/expenses'
 
-test('Should set remove expense action object', () => {
+test('Remove expense action object', () => {
     const action = removeExpense({id: 'abc123'})
     expect(action).toEqual({
         type: 'REMOVE_EXPENSE',
         id: 'abc123'
+    })
+})
+
+test('Edit expense action object', () => {
+    const action = editExpense('qwerty', {})
+    expect(action).toEqual({
+        type: 'EDIT_EXPENSE',
+        id: 'qwerty',
+        updates: {}
     })
 })
