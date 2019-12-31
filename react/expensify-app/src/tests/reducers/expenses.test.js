19247560b8f945c0b96e815f6d34736f875861a1
Robert Bergers <me@robertbergers.com>
2019-12-31 08:52:36 -0800

    remove expense invalid ID test

--- react/expensify-app/src/tests/reducers/expenses.test.js
+++ react/expensify-app/src/tests/reducers/expenses.test.js
@@ -16,3 +16,13 @@ test('Remove expense by ID', () => {
     const state = expensesReducer(expenses, action)
     expect(state).toEqual([expenses[0], expenses[2]])
 })
+
+
+test('Remove no expense if ID not found', () => {
+    const action = {
+        type: 'REMOVE_EXPENSE',
+        id: '-100'
+    }
+    const state = expensesReducer(expenses, action)
+    expect(state).toEqual(expenses)
+})
