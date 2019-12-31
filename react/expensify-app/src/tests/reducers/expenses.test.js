cc917cabad9f60822294806719e8855489765d28
Robert Bergers <me@robertbergers.com>
2019-12-31 08:51:20 -0800

    remove expense by id test

--- react/expensify-app/src/tests/reducers/expenses.test.js
+++ react/expensify-app/src/tests/reducers/expenses.test.js
@@ -1,7 +1,18 @@
 import expensesReducer from '../../reducers/expenses'
+import expenses from '../fixtures/expenses'
 
 
 test('Set default state', () => {
     const state = expensesReducer(undefined, {type: '@@INIT'})
     expect(state).toEqual([])
+})
+
+
+test('Remove expense by ID', () => {
+    const action = {
+        type: 'REMOVE_EXPENSE',
+        id: expenses[1].id
+    }
+    const state = expensesReducer(expenses, action)
+    expect(state).toEqual([expenses[0], expenses[2]])
 })
