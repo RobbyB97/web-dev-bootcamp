aa8ae2673feb26a01d7a759e5a947560a13434c8
Robert Bergers <me@robertbergers.com>
2019-12-31 13:55:09 -0800

    test ExpenseList component

--- react/expensify-app/src/components/ExpenseList.js
+++ react/expensify-app/src/components/ExpenseList.js
@@ -4,7 +4,7 @@ import {connect} from 'react-redux'
 import selectExpenses from '../selectors/expenses'
 import ExpenseListItem from './ExpenseListItem'
 
-const ExpenseList = (props) => (
+export const ExpenseList = (props) => (
   <div>
     <h1>Expense List</h1>
     {props.expenses.map((expense) => (
