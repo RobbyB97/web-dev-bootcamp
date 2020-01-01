54bec1a99e92fd0962ab163fc82dda92dceca1ba
Robert Bergers <me@robertbergers.com>
2020-01-01 15:01:17 -0800

    render ExpenseList without expenses test

--- react/expensify-app/src/tests/components/ExpenseList.test.js
+++ react/expensify-app/src/tests/components/ExpenseList.test.js
@@ -3,7 +3,14 @@ import {shallow} from 'enzyme'
 import {ExpenseList} from '../../components/ExpenseList'
 import expenses from '../fixtures/expenses'
 
+
 test('Render ExpenseList with expenses', () => {
     const wrapper = shallow(<ExpenseList expenses={expenses}/>)
+    expect(wrapper).toMatchSnapshot()
+})
+
+
+test('Render ExpenseList without expenses', () => {
+    const wrapper = shallow(<ExpenseList expenses={[]} />)
     expect(wrapper).toMatchSnapshot()
 })
