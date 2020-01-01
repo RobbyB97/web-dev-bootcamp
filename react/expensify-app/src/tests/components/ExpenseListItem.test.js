3be63479c1c8b5fa4bb5e7b2d6ae6b8edcfbec8f
Robert Bergers <me@robertbergers.com>
2020-01-01 15:17:03 -0800

    ExpenseListItem test initial commit

---
+++ react/expensify-app/src/tests/components/ExpenseListItem.test.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import {shallow} from 'enzyme'
+import {ExpenseListItem} from '../../components/ExpenseListItem'
+import expenseListItems from '../fixtures/expenseListItems'
