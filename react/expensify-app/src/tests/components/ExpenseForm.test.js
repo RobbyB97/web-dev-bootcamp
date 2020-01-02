08bd2924c76281b851f6bbe134f12948aae8e740
Robert Bergers <me@robertbergers.com>
2020-01-02 05:50:53 -0800

    ExpenseForm component test with expense data

--- react/expensify-app/src/tests/components/ExpenseForm.test.js
+++ react/expensify-app/src/tests/components/ExpenseForm.test.js
@@ -2,8 +2,16 @@ import React from 'react'
 import {shallow} from 'enzyme'
 
 import ExpenseForm from '../../components/ExpenseForm'
+import expenses from '../fixtures/expenses'
+
 
 test('Render ExpenseForm', () => {
     const wrapper = shallow(<ExpenseForm />)
+    expect(wrapper).toMatchSnapshot()
+})
+
+
+test('Render ExpenseForm with expenses', () => {
+    const wrapper = shallow(<ExpenseForm expense={expenses[1]} />)
     expect(wrapper).toMatchSnapshot()
 })
