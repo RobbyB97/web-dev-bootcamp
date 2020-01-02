c72aa30ee2dbf31c9e35b74ba5827eb23d997d88
Robert Bergers <me@robertbergers.com>
2020-01-02 05:11:11 -0800

    NotFoundPage component snapshot test

--- react/expensify-app/src/tests/components/NotFoundPage.test.js
+++ react/expensify-app/src/tests/components/NotFoundPage.test.js
@@ -1,3 +1,9 @@
 import React from 'react'
 import {shallow} from 'enzyme'
 import NotFoundPage from '../../components/NotFoundPage'
+
+
+test('Render NotFoundPage', () => {
+    const wrapper = shallow(<NotFoundPage />)
+    expect(wrapper).toMatchSnapshot()
+})
