1d81720e907ff24950e7074270f20565a750cf41
Robert Bergers <me@robertbergers.com>
2020-01-02 05:05:19 -0800

    add ExpenseListItem component shallow test

--- react/expensify-app/src/tests/components/ExpenseListItem.test.js
+++ react/expensify-app/src/tests/components/ExpenseListItem.test.js
@@ -1,4 +1,19 @@
 import React from 'react'
 import {shallow} from 'enzyme'
-import {ExpenseListItem} from '../../components/ExpenseListItem'
+import ExpenseListItem from '../../components/ExpenseListItem'
 import expenseListItems from '../fixtures/expenseListItems'
+
+test('Render ExpenseListItem', () => {
+    const data = {
+        id: expenseListItems[0].id,
+        description: expenseListItems[0].description,
+        amount: expenseListItems[0].amount,
+        createdAt: expenseListItems[0].createdAt
+    }
+    const wrapper = shallow(<ExpenseListItem 
+        id={data.id}
+        description={data.description}
+        amount={data.amount}
+        createdAt={data.createdAt}
+    />)
+})
