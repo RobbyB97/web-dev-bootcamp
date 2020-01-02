a2f38c917eae357df09001887a0d94b6b66726b6
Robert Bergers <me@robertbergers.com>
2020-01-02 06:08:14 -0800

    test description input change

--- react/expensify-app/src/tests/components/ExpenseForm.test.js
+++ react/expensify-app/src/tests/components/ExpenseForm.test.js
@@ -15,3 +15,25 @@ test('Render ExpenseForm with expenses', () => {
     const wrapper = shallow(<ExpenseForm expense={expenses[1]} />)
     expect(wrapper).toMatchSnapshot()
 })
+
+
+test('Render error on invalid form submission', () => {
+    const wrapper = shallow(<ExpenseForm />)
+    wrapper.find('form').simulate('submit', {
+        preventDefault: () => { }
+    })
+
+    expect(wrapper.state('error').length).toBeGreaterThan(0)
+    expect(wrapper).toMatchSnapshot()
+})
+
+
+test('Set description on input change', () => {
+    const wrapper = shallow(<ExpenseForm />)
+    const value = 'New description'
+    wrapper.find('input').at(0).simulate('change', {
+        target: {value}
+    })
+
+    expect(wrapper.state('description')).toBe(value)
+})
