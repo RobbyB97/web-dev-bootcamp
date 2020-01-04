4a1a81c62f298dd5e06002fe3951a84fa380e49f
Robert Bergers <me@robertbergers.com>
2020-01-04 13:44:36 -0800

    ExpenseEdit tests initial commit

---
+++ react/expensify-app/src/tests/components/ExpenseEdit.test.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {shallow} from 'enzyme'
+
+import {ExpenseEdit} from '../../components/ExpenseEdit'
+import expenses from '../fixtures/expenses'
+
+let editExpense, history, wrapper
+
+// Set up ExpenseEdit test environment
+beforeEach(() => {
+    editExpense = jest.fn()
+    history = {push: jest.fn()}
+    wrapper = shallow(<ExpenseEdit editExpense={editExpense} history={history} />)
+})
+
+
+test('Render ExpenseEdit', () => {
+    expect(wrapper).toMatchSnapshot()
+})
