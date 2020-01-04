711453f3008075d24ec437bb091e174892c25c1d
Robert Bergers <me@robertbergers.com>
2020-01-04 11:54:24 -0800

    Refactor: test environment setup

--- react/expensify-app/src/tests/components/ExpenseCreate.test.js
+++ react/expensify-app/src/tests/components/ExpenseCreate.test.js
@@ -5,21 +5,24 @@ import {ExpenseCreate} from '../../components/ExpenseCreate'
 import expenses from '../fixtures/expenses'
 
 
-test('Render ExpenseCreate', () => {
-    const onSubmit = jest.fn()
-    const history = {push: jest.fn()}
-    const wrapper = shallow(<ExpenseCreate onSubmit={onSubmit} history={history} />)
+let onSubmit, history, wrapper
 
+// Set up ExpenseCreate test environment
+beforeEach(() => {
+    onSubmit = jest.fn()
+    history = {push: jest.fn()}
+    wrapper = shallow(<ExpenseCreate onSubmit={onSubmit} history={history} />)
+})
+
+
+test('Render ExpenseCreate', () => {
     expect(wrapper).toMatchSnapshot()
 })
 
 
 test('Handle onSubmit', () => {
-    const onSubmit = jest.fn()
-    const history = {push: jest.fn()}
-    const wrapper = shallow(<ExpenseCreate onSubmit={onSubmit} history={history} />)
-
     wrapper.find('ExpenseForm').prop('onSubmit')(expenses[1])
+    
     expect(history.push).toHaveBeenLastCalledWith('/')
     expect(onSubmit).toHaveBeenLastCalledWith(expenses[1])
 })
