14665736f62ee6e68a8210292196dabdf64d3e12
Robert Bergers <me@robertbergers.com>
2020-01-04 11:51:56 -0800

    handle onSubmit test

--- react/expensify-app/src/tests/components/ExpenseCreate.test.js
+++ react/expensify-app/src/tests/components/ExpenseCreate.test.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import {shallow} from 'enzyme'
+
 import {ExpenseCreate} from '../../components/ExpenseCreate'
+import expenses from '../fixtures/expenses'
 
 
 test('Render ExpenseCreate', () => {
@@ -11,3 +13,13 @@ test('Render ExpenseCreate', () => {
     expect(wrapper).toMatchSnapshot()
 })
 
+
+test('Handle onSubmit', () => {
+    const onSubmit = jest.fn()
+    const history = {push: jest.fn()}
+    const wrapper = shallow(<ExpenseCreate onSubmit={onSubmit} history={history} />)
+
+    wrapper.find('ExpenseForm').prop('onSubmit')(expenses[1])
+    expect(history.push).toHaveBeenLastCalledWith('/')
+    expect(onSubmit).toHaveBeenLastCalledWith(expenses[1])
+})
