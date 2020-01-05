450591e46c3708ee76b29263d3d82cb8fc41254a
Robert Bergers <me@robertbergers.com>
2020-01-05 08:21:08 -0800

    refactor ExpenseListFilters component, add mapDispatchToProps

--- react/expensify-app/src/components/ExpenseListFilters.js
+++ react/expensify-app/src/components/ExpenseListFilters.js
@@ -11,12 +11,25 @@ class ExpenseListFilters extends React.Component {
     }
 
     onDatesChange = ({startDate, endDate}) => {
-        this.props.dispatch(setStartDate(startDate))
-        this.props.dispatch(setEndDate(endDate))
+        this.props.setStartDate(startDate)
+        this.props.setEndDate(endDate)
     }
 
     onFocusChange = (focusCalendar) => {
         this.setState(() => ({focusCalendar}))
+    }
+
+    onTextChange = (e) => {
+        this.props.setTextFilter(e.target.value)
+    }
+
+    onSortChange = (e) => {
+        if (e.target.value === "date") {
+            this.props.sortByDate()
+        } 
+        else if (e.target.value === "amount") {
+            this.props.sortByAmount()
+        }
     }
 
     render() {
@@ -25,20 +38,12 @@ class ExpenseListFilters extends React.Component {
                 <input
                     type="text"
                     value={this.props.filters.text}
-                    onChange={(e) => {
-                        this.props.dispatch(setTextFilter(e.target.value))
-                    }}
+                    onChange={this.onTextChange}
                 />
                 
                 <select
                     value={this.props.filters.sortBy}
-                    onChange={(e) => {
-                        if (e.target.value === "date") {
-                        this.props.dispatch(sortByDate())
-                        } else if (e.target.value === "amount") {
-                        this.props.dispatch(sortByAmount())
-                        }
-                    }}
+                    onChange={this.onSortChange}
                 >
                     <option value="date">Date</option>
                     <option value="amount">Amount</option>
@@ -59,10 +64,16 @@ class ExpenseListFilters extends React.Component {
 }
 
 
-const mapStateToProps = (state) => {
-  return {
-    filters: state.filters
-  }
-}
+const mapDispatchToProps = (dispatch) => ({
+    setTextFilter: (text) => dispatch(setTextFilter(text)),
+    sortByDate: () => dispatch(sortByDate()),
+    sortByAmount: () => dispatch(sortByAmount()),
+    setStartDate: (startDate) => dispatch(setStartDate(startDate)),
+    setEndDate: (endDate) => dispatch(setEndDate(endDate))
+})
 
-export default connect(mapStateToProps)(ExpenseListFilters)
+const mapStateToProps = (state) => ({
+    filters: state.filters
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(ExpenseListFilters)
