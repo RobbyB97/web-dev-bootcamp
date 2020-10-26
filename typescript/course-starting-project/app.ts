4dd3b3e72a854310aeb9e9d7eafcd1e77b703355
Robert Bergers <bergersr97@gmail.com>
2020-10-26 09:01:04 -0400

    2-15: type assignment & type inference

--- typescript/course-starting-project/app.ts
+++ typescript/course-starting-project/app.ts
@@ -10,6 +10,6 @@ function add(n1: number, n2: number, showResult: boolean, phrase: string) {
 const number1 = 5;
 const number2 = 6;
 const printResult = true;
-const resultPhrase = 'Result: '
+const resultPhrase = 'Result: ';
 
 add(number1, number2, printResult, resultPhrase);
