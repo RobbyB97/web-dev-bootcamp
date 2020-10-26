53e190d98e2ea74647a3b889e675ebce2024ec13
Robert Bergers <bergersr97@gmail.com>
2020-10-26 09:12:10 -0400

    2-18: arrays

--- typescript/course-starting-project/app.ts
+++ typescript/course-starting-project/app.ts
@@ -1,9 +1,13 @@
-const person: {
-    name: string;
-    age: number;
-} = {
+const person = {
     name: 'Robby',
-    age: 23
+    age: 23,
+    hobbies: ['Coding', 'Cooking']
 };
 
-console.log(person);
+let favoriteActivities: string[];
+favoriteActivities = ['Sports'];
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
+    // console.log(hobby.map()) <- ERROR: Map can't be used on a string!
+}
