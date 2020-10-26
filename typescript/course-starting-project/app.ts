4a72971fb6a902812e87d495e33c21789d14f092
Robert Bergers <bergersr97@gmail.com>
2020-10-26 09:06:40 -0400

    2-16: object types

--- typescript/course-starting-project/app.ts
+++ typescript/course-starting-project/app.ts
@@ -1,15 +1,9 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-    const result = n1 + n2
-    if (showResult) {
-        console.log(phrase + result);
-    } else {
-        return result;
-    }
-}
+const person: {
+    name: string;
+    age: number;
+} = {
+    name: 'Robby',
+    age: 23
+};
 
-const number1 = 5;
-const number2 = 6;
-const printResult = true;
-const resultPhrase = 'Result: ';
-
-add(number1, number2, printResult, resultPhrase);
+console.log(person);
