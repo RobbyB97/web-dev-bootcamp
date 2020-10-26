f14975d993cc2996c48e5f093b69d0e04b090ef2
Robert Bergers <bergersr97@gmail.com>
2020-10-26 09:17:47 -0400

    2-19: working with tuples

--- typescript/course-starting-project/app.ts
+++ typescript/course-starting-project/app.ts
@@ -1,8 +1,19 @@
-const person = {
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string];
+} = {
     name: 'Robby',
     age: 23,
-    hobbies: ['Coding', 'Cooking']
+    hobbies: ['Coding', 'Cooking'],
+    role: [2, 'programmer'],
 };
+
+// person.role.push('admin');
+// person.role[1] = 10;
+
+person.role = [0, 'super programmer'];
 
 let favoriteActivities: string[];
 favoriteActivities = ['Sports'];
