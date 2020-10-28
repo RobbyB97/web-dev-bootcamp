8a3100bae13c4e9e7d1170082581a4d73d57f8c4
Robert Bergers <bergersr97@gmail.com>
2020-10-28 11:02:29 -0400

    3-37: Including & excluding files

--- typescript/course-starting-project/app.ts
+++ typescript/course-starting-project/app.ts
@@ -1,15 +1,3 @@
-let userInput: unknown;
-let userName: string;
+const userName="Robby";
 
-userInput = 5;
-userInput = 'Robby';
-if (typeof userInput === 'string') {
-    userName = userInput;
-}
-
-function generateError(message: string, code: number): never {
-    throw { message: message, errorCode: code };
-}
-
-const result = generateError('An error occurred!', 500);
-console.log(result); // Result never returns anything
+console.log(userName);
