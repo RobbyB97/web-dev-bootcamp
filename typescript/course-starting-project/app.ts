e1dead556dded3aa68627cb40dc7b19b5b2d0a25
Robert Bergers <bergersr97@gmail.com>
2020-10-28 10:35:34 -0400

    2-30: 'never' type

--- typescript/course-starting-project/app.ts
+++ typescript/course-starting-project/app.ts
@@ -6,3 +6,10 @@ userInput = 'Robby';
 if (typeof userInput === 'string') {
     userName = userInput;
 }
+
+function generateError(message: string, code: number): never {
+    throw { message: message, errorCode: code };
+}
+
+const result = generateError('An error occurred!', 500);
+console.log(result); // Result never returns anything
