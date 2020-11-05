e83f91456f5fe70aedbbd98f2d4c133560988dc8
Robert Bergers <bergersr97@gmail.com>
2020-11-05 10:42:55 -0500

    5-62: "Private" and "Public" access modifiers

--- typescript/interfaces/src/app.ts
+++ typescript/interfaces/src/app.ts
@@ -1,5 +1,6 @@
 class Department {
     name: string;
+    private employees: string[] = [];
     
     constructor(n: string) {
         this.name = n;
@@ -8,12 +9,27 @@ class Department {
     describe(this: Department) {
         console.log('Department: ' + this.name);
     }
+
+    addEmployee(employee: string) {
+        this.employees.push(employee);
+    }
+
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
 }
 
 const accounting = new Department('Accounting');
 // console.log(accounting);
+
+accounting.addEmployee('Robby');
+accounting.addEmployee('Jeyson');
+
+//accounting.employees[2] = 'Anna'; //<- Avoid something like this
+
 accounting.describe();
+accounting.printEmployeeInformation();
 
-const accountingCopy = {name: 'accounting copy', describe: accounting.describe};
-
-accountingCopy.describe();
+//const accountingCopy = {name: 'accounting copy', describe: accounting.describe};
+//accountingCopy.describe();
