bdb0e671f2ac2da46d18476670fc1bc1b9db4de9
Robert Bergers <bergersr97@gmail.com>
2020-11-13 16:07:52 -0500

    5-67:  Getters & setters

--- typescript/interfaces/src/app.ts
+++ typescript/interfaces/src/app.ts
@@ -35,11 +35,26 @@ class ITDepartment extends Department {
 }
 
 class AccountingDepartment extends Department {
+    private lastReport: string;
+
+    get mostRecentReport() {
+        if (this.lastReport) {
+            return this.lastReport;
+        }
+        throw new Error('No report found.');
+    }
+
+    set mostRecentReport(value: string) {
+        if (!value) throw new Error('Please add a report!');
+        this.addReport(value);
+    }
+
     constructor(
         id: string,
         private reports: string[]
     ) {
         super(id, 'Accounting');
+        this.lastReport = reports[0];
     }
 
     addEmployee(name: string) {
@@ -49,6 +64,7 @@ class AccountingDepartment extends Department {
 
     addReport(text: string) {
         this.reports.push(text);
+        this.lastReport = text;
     }
 
     printReports() {
@@ -60,6 +76,8 @@ const it = new ITDepartment('adfgsfhg', ['Robby']);
 // console.log(accounting);
 
 const accounting = new AccountingDepartment('adfgsfhg', []);
+
+accounting.mostRecentReport = '';
 accounting.addReport('Something went wrong...')
 accounting.printReports();
 
